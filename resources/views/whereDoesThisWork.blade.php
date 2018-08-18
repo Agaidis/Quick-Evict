a311d22b3f3d5a691505626d2385930162b64c03
Andrew Gaidis <drewgaidis@gmail.com>
2018-08-18 15:21:31 -0400

    added text

--- resources/views/whereDoesThisWork.blade.php
+++ resources/views/whereDoesThisWork.blade.php
@@ -9,12 +9,42 @@
                 <div class="card-header"><h2>Where does this work?</h2></div>
                 <div class="card-body">
                     <div class="row">
-                        <div>Thanks for your interest in QuickEvict.&nbsp; The goal for Quick Evict is to eventually make evictions easier for owners and landlords in all communities in the United States.&nbsp; Currently, QuickEvict is in beta and only works in Central Pennsylvania in the following counties:&nbsp; Lancaster, Lebanon, Dauphin, Cumberland, York, Chester.&nbsp; More counties will be rolling out shortly.&nbsp;&nbsp;</div>
+                        <div>Thanks for your interest in QuickEvict.</div>
+                        <div>The goal for Quick Evict is to eventually make evictions easier for owners and landlords in all communities in the United States.&nbsp;</div>
+                        <div>Currently, QuickEvict is in beta and only works in Central Pennsylvania in the following counties:</div>
+                        <div>&nbsp;</div>
+                        <div>
+                            <table style="border-collapse: collapse; width: 27.6422%; height: 114px;" border="1">
+                                <tbody>
+                                <tr style="height: 18px;">
+                                    <td style="width: 50%; height: 18px;">Lancaster</td>
+                                </tr>
+                                <tr style="height: 18px;">
+                                    <td style="width: 50%; height: 18px;">Lebanon</td>
+                                </tr>
+                                <tr style="height: 18px;">
+                                    <td style="width: 50%; height: 18px;">Dauphin</td>
+                                </tr>
+                                <tr>
+                                    <td style="width: 50%;">Cumberland</td>
+                                </tr>
+                                <tr>
+                                    <td style="width: 50%;">York</td>
+                                </tr>
+                                <tr>
+                                    <td style="width: 50%;">Chester</td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div>&nbsp;</div>
+                        <div>More counties will be rolling out shortly.&nbsp;&nbsp;</div>
                         <div>&nbsp;</div>
                         <div>If you would like updates as more counties are rolled out, please submit your email here:</div>
                         <div>&nbsp;</div>
-                        <div>Email Address:____________</div>
-                        <div>Area of interest:____________</div>
+                        <div>Email Address:&nbsp; &nbsp;____________</div>
+                        <div>Area of interest:&nbsp; ____________</div>
+                        <p>&nbsp;</p>
                     </div>
                </div>
             </div>
