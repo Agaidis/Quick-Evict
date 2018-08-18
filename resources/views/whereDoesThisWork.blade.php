0b221cae7111653e862d515146ddf80fbb19a62d
Andrew Gaidis <drewgaidis@gmail.com>
2018-08-18 15:15:53 -0400

    added text

--- resources/views/whereDoesThisWork.blade.php
+++ resources/views/whereDoesThisWork.blade.php
@@ -9,7 +9,12 @@
                 <div class="card-header"><h2>Where does this work?</h2></div>
                 <div class="card-body">
                     <div class="row">
-                        Pennsylvania 
+                        <div>Thanks for your interest in QuickEvict.&nbsp; The goal for Quick Evict is to eventually make evictions easier for owners and landlords in all communities in the United States.&nbsp; Currently, QuickEvict is in beta and only works in Central Pennsylvania in the following counties:&nbsp; Lancaster, Lebanon, Dauphin, Cumberland, York, Chester.&nbsp; More counties will be rolling out shortly.&nbsp;&nbsp;</div>
+                        <div>&nbsp;</div>
+                        <div>If you would like updates as more counties are rolled out, please submit your email here:</div>
+                        <div>&nbsp;</div>
+                        <div>Email Address:____________</div>
+                        <div>Area of interest:____________</div>
                     </div>
                </div>
             </div>
