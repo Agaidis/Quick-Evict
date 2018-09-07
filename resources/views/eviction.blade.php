bd836cfac634015c378b2b2ea756ff4d8be40e57
Andrew Gaidis <drewgaidis@gmail.com>
2018-09-06 22:47:21 -0400

    showing pdf form

--- resources/views/eviction.blade.php
+++ resources/views/eviction.blade.php
@@ -162,7 +162,7 @@
 
                             </div>
                             </form>
-                            <form class="form-horizontal" role="form" method="POST" id="request-form" enctype="multipart/form-data" action={{ url('/add-file') }} >
+                            <form class="form-horizontal" role="form" method="POST" id="request-form" enctype="multipart/form-data" action={{ url('/online-eviction/add-file') }} >
                                 <input type="hidden" name="_token" value="{{ Session::token() }}">
                                 <input type="file" name="pdf"/>
                                 <button id="add_file_btn" type="button" class="btn btn-success">Submit File</button>
