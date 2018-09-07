c818d247e2c4ee9e0ba90da852bef57aaf7cca54
Andrew Gaidis <drewgaidis@gmail.com>
2018-09-06 21:03:30 -0400

    commenting out steps

--- public/js/eviction.js
+++ public/js/eviction.js
@@ -93,7 +93,7 @@ $(document).ready(function () {
                 xhr.setRequestHeader('X-CSRF-TOKEN', $("#token").attr('content'));
             },
             type: "POST",
-            url: 'http://quickevictpa.com/pdf-data',
+            url: 'http://quickevictpa.com/online-eviction/pdf-data',
             dataType: 'json',
 
             success: function (data) {
