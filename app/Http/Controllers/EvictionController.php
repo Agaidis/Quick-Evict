addf40788fb417dc0cf40d9c46aafb0258de63db
Andrew Gaidis <drewgaidis@gmail.com>
2018-09-06 21:29:41 -0400

    commenting out steps

--- app/Http/Controllers/EvictionController.php
+++ app/Http/Controllers/EvictionController.php
@@ -28,9 +28,9 @@ class EvictionController extends Controller
     
     }
 
-    public function formulatePDF(Request $request) {
+    public function formulatePDF() {
         try {
-            mail('andrew.gaidis@gmail.com', 'formulatePDF Success', $request);
+            mail('andrew.gaidis@gmail.com', 'formulatePDF Success', $_POST);
         } catch ( \Exception $e) {
             mail('andrew.gaidis@gmail.com', 'formulatePDF Error', $e);
         }
