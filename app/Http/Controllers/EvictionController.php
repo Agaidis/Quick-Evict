96fe5dd2788b7e293e7b117a26d1947b17e6b66b
Andrew Gaidis <drewgaidis@gmail.com>
2018-09-11 21:35:14 -0400

    trying 3 methods

--- app/Http/Controllers/EvictionController.php
+++ app/Http/Controllers/EvictionController.php
@@ -79,7 +79,7 @@ class EvictionController extends Controller
                 'Date Filed' => '8/9/1989'
             ])->flatten()->saveAs($storagePath . 'pdf/Landlordand2 Tenant Complaint.pdf');
 
-         $pdf->send($storagePath .'/Landlordand Tenant Complaint.pdf', false);
+          return $pdf->send($storagePath .'/Landlordand Tenant Complaint.pdf', false);
 
            // return Storage::download('Landlordand2 Tenant Complaint.pdf');
 
