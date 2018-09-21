a22208f6a1b008bed6c93ca9ddfb24612545802e
Andrew Gaidis <drewgaidis@gmail.com>
2018-09-20 22:59:46 -0400

    adding background image

--- app/Http/Controllers/EvictionController.php
+++ app/Http/Controllers/EvictionController.php
@@ -23,8 +23,36 @@ class EvictionController extends Controller
 //            return view('/login');
 //        } else {
 // instantiate and use the dompdf class
-        $dompdf = new Dompdf();
-        $dompdf->loadHtml('<html>
+
+            return view('eviction', compact('map'));
+     //   }
+    }
+    
+    //When they click next step.. call this function to take their address and turn it into the maps view
+    public function getMapLocation() {
+    
+    }
+
+    public function formulatePDF() {
+        try {
+            $additionalRent = $_POST['addit_rent'];
+            $attorneyFees = $_POST['attorney_fees'];
+            $damageAmt = $_POST['damage_amt'];
+            $filing_date = $_POST['filing_date'];
+            $landlord = $_POST['landlord'];
+            $leaseStatus = $_POST['lease_status'];
+            $leaseType = $_POST['lease_type'];
+            $ownerName = $_POST['owner_name'];
+            $ownerPhone = $_POST['owner_phone'];
+            $propertyAddressLine1 = $_POST['property_address_1'];
+            $propertyAddressLine2 = $_POST['property_address_2'];
+            $quitNotice = $_POST['quit_notice'];
+            $rentedBy = $_POST['rented_by'];
+            $termLease = $_POST['term_lease'];
+            $unjustDamages = $_POST['unjust_damages'];
+
+            $dompdf = new Dompdf();
+            $dompdf->loadHtml('<html>
 <head><meta http-equiv=Content-Type content="text/html; charset=UTF-8">
 <style type="text/css">
 <!--
@@ -142,52 +170,13 @@ span.cls_010{font-family:Arial,serif;font-size:8.1px;color:rgb(0,0,0);font-weigh
 </span></body></html>');
 
 // (Optional) Setup the paper size and orientation
-        $dompdf->setPaper('A4', 'portrait');
+            $dompdf->setPaper('A4', 'portrait');
 
 // Render the HTML as PDF
-        $dompdf->render();
+            $dompdf->render();
 
 // Output the generated PDF to Browser
-        $dompdf->stream();
-            return view('eviction', compact('map'));
-     //   }
-    }
-    
-    //When they click next step.. call this function to take their address and turn it into the maps view
-    public function getMapLocation() {
-    
-    }
-
-    public function formulatePDF() {
-        try {
-            $additionalRent = $_POST['addit_rent'];
-            $attorneyFees = $_POST['attorney_fees'];
-            $damageAmt = $_POST['damage_amt'];
-            $filing_date = $_POST['filing_date'];
-            $landlord = $_POST['landlord'];
-            $leaseStatus = $_POST['lease_status'];
-            $leaseType = $_POST['lease_type'];
-            $ownerName = $_POST['owner_name'];
-            $ownerPhone = $_POST['owner_phone'];
-            $propertyAddressLine1 = $_POST['property_address_1'];
-            $propertyAddressLine2 = $_POST['property_address_2'];
-            $quitNotice = $_POST['quit_notice'];
-            $rentedBy = $_POST['rented_by'];
-            $termLease = $_POST['term_lease'];
-            $unjustDamages = $_POST['unjust_damages'];
-
-// instantiate and use the dompdf class
-$dompdf = new Dompdf();
-$dompdf->loadHtml('<span>Fuck off</span>');
-
-// (Optional) Setup the paper size and orientation
-$dompdf->setPaper('A4', 'landscape');
-
-// Render the HTML as PDF
-$dompdf->render();
-
-// Output the generated PDF to Browser
-$dompdf->stream();
+            $dompdf->stream();
 
 
         } catch ( \Exception $e) {
