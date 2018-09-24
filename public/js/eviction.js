1f72895a8a3e2dfa50d05b05e8a6c8012842d657
Andrew Gaidis <drewgaidis@gmail.com>
2018-09-23 20:54:17 -0400

    committin gprogress

--- public/js/eviction.js
+++ public/js/eviction.js
@@ -36,12 +36,7 @@ $(document).ready(function () {
         });
         bounds = new google.maps.LatLngBounds();
         google.maps.event.addListenerOnce(map, 'tilesloaded', function(evt) {
-            var place = autocomplete.getPlace();
 
-            alert(place.address_components[0].long_name);
-            alert(place.address_components[1].long_name);
-            alert(place.address_components[2].long_name);
-            alert(place.address_components[2].long_name);
             bounds = map.getBounds();
         });
         marker = new google.maps.Marker({
@@ -88,6 +83,10 @@ $(document).ready(function () {
                 window.alert("Returned place contains no geometry");
                 return;
             };
+            alert(place.address_components[0].long_name);
+            alert(place.address_components[1].long_name);
+            alert(place.address_components[2].long_name);
+            alert(place.address_components[2].long_name);
             marker.setPosition(place.geometry.location);
             marker.setMap(map);
             newBounds.extend(place.geometry.location);
