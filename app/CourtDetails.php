e1d0c50218de89644aa57097356a4bb0c1d2e91e
Andrew Gaidis <drewgaidis@gmail.com>
2018-10-02 21:01:04 -0400

    adding values to DB

--- app/CourtDetails.php
+++ app/CourtDetails.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class CourtDetails extends Model
 {
+    protected $fillable = [
+        'county',
+        'court_number',
+        '1_defendant_up_to_2000',
+        '2_defendant_up_to_2000',
+        '1_defendant_between_2001_4000',
+        '2_defendant_between_2001_4000',
+        '1_defendant_greater_than_4000',
+        '2_defendant_greater_than_4000',
+        'out_of_pocket',
+        'mailing_address',
+        'phone_number',
+        'accept_e_signature'
+    ];
+
     protected $table = 'court_details';
 }
