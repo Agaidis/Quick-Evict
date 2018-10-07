1a37c6a57c09047a49e3a0c2bd23356ef9654090
Andrew Gaidis <drewgaidis@gmail.com>
2018-10-07 15:48:19 -0400

    committing progress

--- resources/views/eviction.blade.php
+++ resources/views/eviction.blade.php
@@ -95,7 +95,7 @@
                                                             <label for="term_lease_ended">Term of lease has ended</label><br>
                                                             <input type="checkbox" id="breached_conditions_lease" name="breached_conditions_lease">
                                                             <label for="breached_conditions_lease">Tenant Breached Conditions of Lease</label>
-                                                            <span id="details"></span><b>Details:</b><input type="text" id="breached_details" name="breached_details" /><br>
+                                                            <div id="details"></div><b>Details:</b><input class="form-control" type="text" id="breached_details" name="breached_details" /><br>
                                                             <input type="checkbox" id="unsatisfied_lease" name="unsatisfied_lease">
                                                             <label for="unsatisfied_lease">Rent Reserved and due has, upon demand, remained unsatisfied</label>
                                                         </div>
