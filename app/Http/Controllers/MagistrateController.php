6da0ae11935c83fee3019d35b7a614ac5b60233b
Andrew Gaidis <drewgaidis@gmail.com>
2018-10-08 16:47:18 -0400

    committing porgress

--- app/Http/Controllers/MagistrateController.php
+++ app/Http/Controllers/MagistrateController.php
@@ -2,15 +2,63 @@
 
 namespace App\Http\Controllers;
 
+use App\CourtDetails;
 use Illuminate\Http\Request;
 
 class MagistrateController extends Controller
 {
     public function index() {
-        return view('magistrateCreator');
+        // Get all the series
+        $courtDetails = CourtDetails::all();
+
+        return view('magistrateCreator')->with('courtDetails', $courtDetails);
     }
 
-    public function store() {
+    public function store(Request $request) {
+        try {
+            $tagArray = array();
+            $courtDetails = CourtDetails::all();
+            foreach ($courtDetails as $courtDetail ) {
+                $tagArray[$courtDetail->court_id] = $courtDetail->court_id;
 
+            }
+            $isDuplicate = array_search($request->court_id, $tagArray);
+
+            if ( $isDuplicate == $request->court_id ) {
+                $returnArray['responseMessage'] = 'Duplicate Tag';
+                $returnArray['messageDetails'] = 'Duplicate Tag name AND value combination';
+            } else {
+                $courtDetails = new CourtDetails();
+                $courtDetails->county = $request->county;
+                $courtDetails->court_number = $request->court_id;
+                $courtDetails->one_defendant_up_to_2000 = $request->one_under_2000;
+                $courtDetails->two_defendant_up_to_2000 = $request->two_under_2000;
+                $courtDetails->one_defendant_between_2001_4000 = $request->one_btn_2000_4001;
+                $courtDetails->two_defendant_between_2001_4000 = $request->two_btn_2000_4001;
+                $courtDetails->one_defendant_greater_than_4000 = $request->one_over_4000;
+                $courtDetails->two_defendant_greater_than_4000 = $request->two_over_4000;
+                $courtDetails->one_defendant_out_of_pocket = $request->one_oop;
+                $courtDetails->two_defendant_out_of_pocket = $request->two_oop;
+                $courtDetails->mailing_address = $request->court_address;
+                $courtDetails->mdj_name = $request->mdj_name;
+                $courtDetails->phone_number = $request->court_number;
+                $courtDetails->save();
+            }
+        } catch ( \Exception $e ) {
+            $errorDetails = 'MagistrateController - error in store() method when attempting to store magistrate';
+            $errorDetails .= PHP_EOL . 'File: ' . $e->getFile();
+            $errorDetails .= PHP_EOL . 'Line #' . $e->getLine();
+            \Log::error( $errorDetails . PHP_EOL . 'Error Message: ' . $e->getMessage() . PHP_EOL . 'Trace: ' . $e->getTraceAsString());
+            mail( 'agaidis@moneymappress.com',  'Adding Magistrate Error', $errorDetails );
+
+            $returnArray['responseMessage'] = 'Bad Request';
+            $returnArray['responseCode'] = 400;
+            $returnArray['messageDetails'] = 'Tag could not be added to the database, please try again later';
+            return response()->json($returnArray);
+        }
+        $response['responseMessage'] = 'Adding Magistrate Successful!';
+        $response['messageDetails'] = 'All Good';
+
+        return $response;
     }
 }
