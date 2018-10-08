b6cf50af1b1b63fe0a3ed7b2c9ee6f35ef00bf0f
Andrew Gaidis <drewgaidis@gmail.com>
2018-10-08 15:27:42 -0400

    committing progress

--- resources/views/magistrateCreator.blade.php
+++ resources/views/magistrateCreator.blade.php
@@ -12,69 +12,70 @@
                             <div class="col-sm-10">
                             <form>
                                 <div class="row">
-                                    <div class="col-sm-5">
-                                        <label for="county">County:</label>
-                                        <input type="text" class="form-control" id="county" name="county" value="" />
+                                    <div class="col-md-2">
+                                        <label for="court_id">Court Id:</label>
+                                        <input placeholder="Court Id" type="text" class="form-control" id="court_id" name="court_id" value="" />
                                     </div>
-                                        <div class="col-md-2">
-                                            <label for="court_id">Court Id:</label>
-                                            <input type="text" class="form-control" id="court_id" name="court_id" value="" />
-                                        </div>
+                                    <div class="col-sm-4">
+                                        <label for="county">County:</label>
+                                        <input placeholder="County" type="text" class="form-control" id="county" name="county" value="" />
+                                    </div>
                                 </div>
-
-                                        <div class="col-sm-6">
-                                            <label for="one_under_2000">One Defendant under 2,000</label>
-                                            <input type="text" class="form-control" id="one_under_2000" name="one_under_2000" value="" />
-                                        </div>
-                                        <div class="col-sm-6">
-                                            <label for="two_under_2000">Two Defendant under 2,000</label>
-                                            <input type="text" class="form-control" id="two_under_2000" name="two_under_2000" value="" />
-                                        </div>
-                                        <div class="col-sm-6">
-                                            <label for="one_btn_2000_4001">One Defendant between 2,001 and 4,000</label>
-                                            <input type="text" class="form-control" id="one_btn_2000_4001" name="one_btn_2000_4001" value="" />
-                                        </div>
-                                        <div class="col-sm-6">
-                                            <label for="two_btn_2000_4001">Two Defendants between 2,001 and 4,000</label>
-                                            <input type="text" class="form-control" id="two_btn_2000_4001" name="two_btn_2000_4001" value="" />
-                                        </div>
-                                        <div class="col-sm-6">
-                                            <label for="one_over_4000">One Defendant over 4,000</label>
-                                            <input type="text" class="form-control" id="one_over_4000" name="one_over_4000" value="" />
-                                        </div>
-                                        <div class="col-sm-6">
-                                            <label for="two_over_4000">Two Defendants over 4,000</label>
-                                            <input type="text" class="form-control" id="two_over_4000" name="two_over_4000" value="" />
-                                        </div>
-                                        <div class="col-sm-6">
-                                            <label for="one_oop">One Defendant OOP</label>
-                                            <input type="text" class="form-control" id="one_oop" name="one_oop" value="" />
-                                        </div>
-                                        <div class="col-sm-6">
-                                            <label for="two_oop">Two Defendants OOP</label>
-                                            <input type="text" class="form-control" id="two_oop" name="two_oop" value="" />
-                                        </div>
-                                        <div class="col-sm-8">
-                                            <label for="mdj_name">MDJ Name</label>
-                                            <input type="text" class="form-control" id="mdj_name" name="mdj_name" value="" />
-                                        </div>
-                                        <div class="col-sm-8">
-                                            <label for="court_address">Court Mailing Address</label>
-                                            <input type="text" class="form-control" id="court_address" name="court_address" value="" />
-                                        </div>
-                                        <div class="col-sm-4">
-                                            <label for="mdj_name">Court Phone Number</label>
-                                            <input type="text" class="form-control" id="court_number" name="court_number" value="" />
-                                        </div>
-                                        <div class="col-sm-6">
-                                            <label for="mdj_name">Latitudes</label>
-                                            <textarea style="height:10%;" placeholder="Latitudes" type="text" class="form-control" id="latitudes" name="latitudes"></textarea>
-                                        </div>
-                                        <div class="col-sm-6">
-                                            <label for="mdj_name">Longitudes</label>
-                                            <textarea style="height:10%;" placeholder="Longitudes" type="text" class="form-control" id="longitudes" name="longitudes"></textarea>
-                                        </div>
-                                        <input class="btn btn-primary" type="submit" value="Submit Magistrate" name="submit_btn"/>
+                                <div class="row">
+                                    <div class="col-sm-5">
+                                        <label for="one_under_2000">One Defendant under 2,000</label>
+                                        <input placeholder="$$$" type="text" class="form-control" id="one_under_2000" name="one_under_2000" value="" />
+                                    </div>
+                                    <div class="col-sm-5">
+                                        <label for="two_under_2000">Two Defendant under 2,000</label>
+                                        <input placeholder="$$$" type="text" class="form-control" id="two_under_2000" name="two_under_2000" value="" />
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-sm-5">
+                                        <label for="one_btn_2000_4001">One Defendant between 2,001 and 4,000</label>
+                                        <input placeholder="$$$" type="text" class="form-control" id="one_btn_2000_4001" name="one_btn_2000_4001" value="" />
+                                    </div>
+                                    <div class="col-sm-5">
+                                        <label for="two_btn_2000_4001">Two Defendants between 2,001 and 4,000</label>
+                                        <input placeholder="$$$" type="text" class="form-control" id="two_btn_2000_4001" name="two_btn_2000_4001" value="" />
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-sm-5">
+                                        <label for="one_over_4000">One Defendant over 4,000</label>
+                                        <input placeholder="$$$" type="text" class="form-control" id="one_over_4000" name="one_over_4000" value="" />
+                                    </div>
+                                    <div class="col-sm-5">
+                                        <label for="two_over_4000">Two Defendants over 4,000</label>
+                                        <input placeholder="$$$" type="text" class="form-control" id="two_over_4000" name="two_over_4000" value="" />
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-sm-5">
+                                        <label for="one_oop">One Defendant OOP</label>
+                                        <input placeholder="$$$" type="text" class="form-control" id="one_oop" name="one_oop" value="" />
+                                    </div>
+                                    <div class="col-sm-5">
+                                        <label for="two_oop">Two Defendants OOP</label>
+                                        <input placeholder="$$$" type="text" class="form-control" id="two_oop" name="two_oop" value="" />
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-sm-5">
+                                        <label for="mdj_name">MDJ Name</label>
+                                        <input placeholder="Tom Pietro" type="text" class="form-control" id="mdj_name" name="mdj_name" value="" />
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <label for="court_address">Court Mailing Address</label>
+                                        <input placeholder="123 Muholland Drive, Lancaster PA 17349" type="text" class="form-control" id="court_address" name="court_address" value="" />
+                                    </div>
+                                    <div class="col-sm-3">
+                                        <label for="mdj_name">Court Phone Number</label>
+                                        <input placeholder="(000)-000-0000" type="text" class="form-control" id="court_number" name="court_number" value="" />
+                                    </div>
+                                </div>
+                                <input class="btn btn-primary" type="submit" value="Submit Magistrate" name="submit_btn"/>
                             </form>
                             </div>
                         </div>
