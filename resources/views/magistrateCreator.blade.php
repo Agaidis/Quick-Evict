2445a458cd17a660f00f852de1cd923a705311d2
Andrew Gaidis <drewgaidis@gmail.com>
2018-10-08 16:06:27 -0400

    committing migration

--- resources/views/magistrateCreator.blade.php
+++ resources/views/magistrateCreator.blade.php
@@ -14,28 +14,28 @@
                                 <div class="col-md-4">
                                     <h4 class="major_labels">Court Information</h4>
                                     <div class="court_information_container">
-                                        <div class="col-md-2">
+                                        <div class="col-md-4">
                                             <label for="court_id">Court Id:</label>
                                             <input placeholder="Court Id" type="text" class="form-control" id="court_id" name="court_id" value="" />
                                         </div>
-                                        <div class="col-sm-4">
+                                        <div class="col-sm-6">
                                             <label for="county">County:</label>
                                             <input placeholder="County" type="text" class="form-control" id="county" name="county" value="" />
                                         </div>
                                         <div class="row">
-                                            <div class="col-sm-5">
+                                            <div class="col-sm-8">
                                                 <label for="mdj_name">MDJ Name</label>
                                                 <input placeholder="Tom Pietro" type="text" class="form-control" id="mdj_name" name="mdj_name" value="" />
                                             </div>
                                         </div>
                                         <div class="row">
-                                            <div class="col-sm-6">
+                                            <div class="col-sm-12">
                                                 <label for="court_address">Court Mailing Address</label>
                                                 <input placeholder="123 Muholland Drive, Lancaster PA 17349" type="text" class="form-control" id="court_address" name="court_address" value="" />
                                             </div>
                                         </div>
                                         <div class="row">
-                                            <div class="col-sm-3">
+                                            <div class="col-sm-6">
                                                 <label for="mdj_name">Court Phone Number</label>
                                                 <input placeholder="(000)-000-0000" type="text" class="form-control" id="court_number" name="court_number" value="" />
                                             </div>
