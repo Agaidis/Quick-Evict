7f21085b82bd38fc224726dc78bec1da041fc029
Andrew Gaidis <drewgaidis@gmail.com>
2018-10-08 17:24:52 -0400

    committing fix

--- database/migrations/2018_10_08_212113_fix_problems.php
+++ database/migrations/2018_10_08_212113_fix_problems.php
@@ -15,7 +15,6 @@ class FixProblems extends Migration
     {
         Schema::table('court_details', function (Blueprint $table) {
             $table->dropColumn('court_id');
-            $table->string('court_number')->unique()->change();
         });
     }
 
