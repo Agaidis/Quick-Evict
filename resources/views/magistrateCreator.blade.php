1e2af6c3ab7fd8c48c1942ad8e225e264df71cb5
Andrew Gaidis <drewgaidis@gmail.com>
2018-10-11 20:08:47 -0400

    committing fix

--- resources/views/magistrateCreator.blade.php
+++ resources/views/magistrateCreator.blade.php
@@ -13,7 +13,7 @@
                                 <p class="alert alert-success">{{ Session::get('alert-success') }} <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a></p>
                             @endif
                         </div> <!-- end .flash-message -->
-                        <table class="table table-hover table-responsive-lg table-bordered">
+                        <table class="table table-hover table-responsive-lg table-bordered magistrate_table">
                                     <thead>
                                     <tr>
                                         <th>Magistrate #</th>
