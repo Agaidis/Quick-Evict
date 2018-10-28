44e73bbeef5a1284cd7068bc491838155fa46e9f
Andrew Gaidis <drewgaidis@gmail.com>
2018-10-28 12:36:45 -0400

    committing progress

--- public/js/eviction.js
+++ public/js/eviction.js
@@ -100,7 +100,7 @@ if (document.location.href.split('/')[3] == 'online-eviction') {
             var isFound = false;
             for (var k = 0; k < magArray.length; k++) {
                 if (google.maps.geometry.poly.containsLocation(place.geometry.location, magArray[k])) {
-                    console.log(magArray[k]);
+                    console.log(magArray[k].areaName);
                     $('#court_number').val(magArray[k]);
                     isFound = true;
                 }
