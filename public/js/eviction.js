195d4714df221226b5073f565a84910447e92f23
Andrew Gaidis <drewgaidis@gmail.com>
2018-10-27 21:44:43 -0400

    committing progress

--- public/js/eviction.js
+++ public/js/eviction.js
@@ -48,6 +48,8 @@ if (document.location.href.split('/')[3] == 'online-eviction') {
         console.log(magistrate023046Area);
         console.log(quickEvict.geoData['geo_locations']);
         var test = quickEvict.geoData['geo_locations'].replace(/\s/g, '');
+        test = test.replace(/},/g, '},dd');
+        test = test.split('dd');
         console.log(test);
         test = JSON.parse(test);
 
