fb7ae1ef1c18e26f79acc8308acae7488d7abeff
Andrew Gaidis <drewgaidis@gmail.com>
2018-10-28 12:41:35 -0400

    committing progress

--- app/Http/Controllers/EvictionController.php
+++ app/Http/Controllers/EvictionController.php
@@ -33,11 +33,14 @@ class EvictionController extends Controller
     public function formulatePDF() {
 
         try {
-            $courtNumber = $_POST['court_number'];
-            $courtDetails = CourtDetails::where('court_number', $courtNumber)->first();
+            $magistrateId = str_replace('magistrate_' , '', $_POST['court_number']);
+            $courtDetails = CourtDetails::where('court_number', $magistrateId)->first();
+            $geoDetails = GeoLocation::where('magistrate_id', $magistrateId)->first();
 
-            $courtAddressLine1 = $_POST['court_address1'];
-            $courtAddressLine2 = $_POST['court_address2'];
+            $courtNumber = $courtDetails->court_number;
+
+            $courtAddressLine1 = $geoDetails->address_line_one;
+            $courtAddressLine2 = $geoDetails->address_line_two;
 
             $attorneyFees = $_POST['attorney_fees'];
             $attorneyFees = str_replace('$', '', $attorneyFees);
@@ -189,13 +192,13 @@ span.cls_010{font-family:Arial,serif;font-size:8.1px;color:rgb(0,0,0);font-weigh
 <span style="position:absolute;left:36.05px;top:29.55px" class="cls_003"><span class="cls_003">COUNTY OF ' . strtoupper($courtDetails->county) .'</span></span><br>
 <span style="position:absolute;left:336.30px;top:67.80px" class="cls_005"><span class="cls_005">PLAINTIFF:</span><br><p style="margin-left:6px;">'. $plaintiffLine .'<br>PO Box 5304<br>Lancaster, PA 17606</p></span><br>
 <span style="position:absolute;left:463.70px;top:68.50px" class="cls_005"><span class="cls_005">NAME and ADDRESS</span></span>
-<span style="position:absolute;left:40.80px;top:69.36px" class="cls_004"><span class="cls_004">Mag. Dist. No: '. $courtDetails->court_number.'</span></span><br>
+<span style="position:absolute;left:40.80px;top:69.36px" class="cls_004"><span class="cls_004">Mag. Dist. No: '. $courtNumber .'</span></span><br>
 <span style="position:absolute;left:40.90px;top:82.85px" class="cls_004"><span class="cls_004">MDJ Name: '. $courtDetails->mdj_name .'</span></span><br>
 <span style="position:absolute;left:40.90px;top:101.05px" class="cls_004"><span class="cls_004">Address: '.$courtAddressLine1.'<br><span style="margin-left:34px;">'.$courtAddressLine2.'</span></span></span><br>
 <span style="position:absolute;left:437.10px;top:130.90px" class="cls_006"><span class="cls_006">V.</span></span><br>
 <span style="position:absolute;left:336.30px;top:133.60px" class="cls_009"><span class="cls_009">DEFENDANT:</span><br><p style="margin-left:6px;">'.$tenantName.'<br>'.$defendanthouseNum.' '.$defendantStreetName.'<br>'.$defendantTown .',' . $defendantState.' '.$defendantZipcode.'</p></span><br>
 <span style="position:absolute;left:466.50px;top:135.00px" class="cls_005"><span class="cls_005">NAME and ADDRESS</span></span><br>
-<span style="position:absolute;left:40.90px;top:144.45px" class="cls_004"><span class="cls_004">Telephone: '.$courtDetails->court_number.'</span></span><br>
+<span style="position:absolute;left:40.90px;top:144.45px" class="cls_004"><span class="cls_004">Telephone: '.$courtDetails->phone_number.'</span></span><br>
 <span style="position:absolute;left:142.45px;top:160.95px" class="cls_004"><span class="cls_004">AMOUNT</span></span><br>
 <span style="position:absolute;left:229.35px;top:160.95px" class="cls_004"><span class="cls_004">DATE PAID</span></span><br>
 <span style="position:absolute;left:38.80px;top:174.00px" class="cls_004"><span class="cls_004">FILING COSTS:</span></span><br>
