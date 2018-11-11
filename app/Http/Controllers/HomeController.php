d00bcb3aee609e012ecf259097235aefec86265a
Andrew Gaidis <drewgaidis@gmail.com>
2018-11-11 17:05:21 -0500

    committing migration

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,7 +24,16 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $evictions = Evictions::all();
-        return view('home', compact('evictions'));
+        try {
+            $evictions = Evictions::all();
+            return view('home', compact('evictions'));
+        } catch ( \Exception $e ) {
+            $errorDetails = 'HomeController - error in store() method when attempting to store magistrate';
+            $errorDetails .= PHP_EOL . 'File: ' . $e->getFile();
+            $errorDetails .= PHP_EOL . 'Line #' . $e->getLine();
+            \Log::error( $errorDetails . PHP_EOL . 'Error Message: ' . $e->getMessage() . PHP_EOL . 'Trace: ' . $e->getTraceAsString());
+            mail( 'andrew.gaidis@gmail.com',  'Showing Home Page', $errorDetails );
+        }
+
     }
 }
