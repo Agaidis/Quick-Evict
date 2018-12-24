335a07246afec81384cc298e5b83904c2aaab399
Andrew Gaidis <drewgaidis@gmail.com>
2018-12-24 10:52:21 -0500

    committing comment

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,7 +3,7 @@
     <meta name="csrf-token" id="token" content="{{ csrf_token() }}">
     <div class="container-fluid">
         <div class="row justify-content-center">
-            <div class="col-md-10">
+            <div class="col-md-12">
                 <div class="card">
                     <div class="card-header">Dashboard</div>
                     <div class="card-body">
