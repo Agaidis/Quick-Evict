4048e92ebf88852db78311104afa5f0c055081ea
Andrew Gaidis <drewgaidis@gmail.com>
2019-01-20 20:53:38 -0500

    committing this

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -34,7 +34,7 @@ class DashboardController extends Controller
 
             $userId = Auth::user()->id;
             $courtNumber = Auth::user()->court_id;
-            mail('andrew.gaidis@gmail.com', 'db error', Auth::user()->role);
+            mail('andrew.gaidis@gmail.com', 'db error', $courtNumber);
 
             if (Auth::user()->role == 'Administrator') {
                 $evictions = DB::select('select * from evictions ORDER BY FIELD(status, "Created LTC", "LTC Mailed", "LTC Submitted Online", "Court Hearing Scheduled", "Court Hearing Extended", "Judgement Issued in Favor of Owner", "Judgement Denied by Court", "Tenant Filed Appeal", "OOP Mailed", "OOP Submitted Online", "Paid Judgement", "Locked Out Tenant"), id DESC');
