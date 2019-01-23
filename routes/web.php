9e7d076f1ff99512c526453aa68ea5d736157bbd
Andrew Gaidis <agaidis@moneymappress.com>
2019-01-23 15:47:47 -0500

    ability to change court

--- routes/web.php
+++ routes/web.php
@@ -50,7 +50,7 @@ Route::post('/magistrateCreator/delete', 'MagistrateController@delete')->middlew
 Route::get('/userManagement', 'UserManagementController@index')->middleware('auth');
 Route::post('/userManagement/deleteUser', 'UserManagementController@deleteUser')->middleware('auth');
 Route::post('/userManagement/changeRole', 'UserManagementController@changeUserRole')->middleware('auth');
-Route::post('/userManagement/changeRole', 'UserManagementController@changeCourt')->middleware('auth');
+Route::post('/userManagement/changeCourt', 'UserManagementController@changeCourt')->middleware('auth');
 
 
 
