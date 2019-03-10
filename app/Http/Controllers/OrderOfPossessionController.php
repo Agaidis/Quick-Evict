b4ed51876e78536ef37aadb520bd1b1dfce6fe26
Andrew Gaidis <drewgaidis@gmail.com>
2019-03-10 19:12:19 -0400

    altering inline css

--- app/Http/Controllers/OrderOfPossessionController.php
+++ app/Http/Controllers/OrderOfPossessionController.php
@@ -129,7 +129,7 @@ span.cls_009{font-family:Arial,serif;font-size:9.31px;color:rgb(0,0,0);font-weig
 <span style="position:absolute;left:50.90px;top:134px" class="cls_004"><span class="cls_004">MDJ Name: '. $courtDetails->mdj_name .'</span></span>
 <span style="position:absolute;left:450.90px;top:120px" class="cls_005"><span class="cls_005">PLANTIFF:</span><br><p style="margin-left:6px;">'. $plantiffName .'<br>'. $plantiffAddress1 .'<br>'. $plantiffAddress2 .'<br>'.$plantiffPhone.'</p></span>
 <span style="position:absolute;left:500.90px;top:145px" class="cls_005"><span class="cls_005">V.</span></span>
-<span style="position:absolute;left:450.90px;top:170px" class="cls_005"><span class="cls_005">DEFENDANT:</span><br><p style="margin-left:6px;">'. $_POST['tenant_name'].'<br>'.$defendanthouseNum.' '.$defendantStreetName.' '. $_POST['unit_number'] . '<br>'.$defendantTown .',' . $defendantState.' '.$defendantZipcode.'  </p></span>
+<span style="position:absolute;left:450.90px;top:180px" class="cls_005"><span class="cls_005">DEFENDANT:</span><br><p style="margin-left:6px;">'. $_POST['tenant_name'].'<br>'.$defendanthouseNum.' '.$defendantStreetName.' '. $_POST['unit_number'] . '<br>'.$defendantTown .',' . $defendantState.' '.$defendantZipcode.'  </p></span>
 <span style="position:absolute;left:50.90px;top:180px" class="cls_004"><span class="cls_004">Address: '.$courtAddressLine1.'<br><span style="margin-left:47px;">'.$courtAddressLine2.'</span></span>
 
 <span style="position:absolute;left:395.89px;top:210px" class="cls_004"><span class="cls_004">Docket No:</span></span>
