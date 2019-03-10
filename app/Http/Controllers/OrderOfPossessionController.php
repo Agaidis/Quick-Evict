c438ea25077578b822136d687d4269e88ccbf8ab
Andrew Gaidis <drewgaidis@gmail.com>
2019-03-10 18:10:51 -0400

    altering inline css

--- app/Http/Controllers/OrderOfPossessionController.php
+++ app/Http/Controllers/OrderOfPossessionController.php
@@ -136,35 +136,30 @@ span.cls_009{font-family:Arial,serif;font-size:9.31px;color:rgb(0,0,0);font-weig
 <span style="position:absolute;left:394.89px;top:225.80px" class="cls_004"><span class="cls_004">Case Filed:</span></span>
 <span style="position:absolute;left:395.84px;top:240.80px" class="cls_004"><span class="cls_004">Time Filed:</span></span>
 <span style="position:absolute;left:371.27px;top:255.80px" class="cls_004"><span class="cls_004">Date Order Filed:</span></span>
-<span style="position:absolute;left:108.00px;top:327.75px" class="cls_004"><span class="cls_004">Judgment Amount</span></span>
+<span style="position:absolute;left:98.00px;top:327.75px" class="cls_004"><span class="cls_004">Judgment Amount</span></span>
 <span style="position:absolute;left:192.00px;top:327.55px" class="cls_003"><span class="cls_003">$_</span>'. $_POST['judgment_amount'] .'</span>
-<span style="position:absolute;left:290.68px;top:327.55px" class="cls_003"><span class="cls_003">_</span></span>
-<span style="position:absolute;left:54.00px;top:339.75px" class="cls_004"><span class="cls_004">Costs in Original LT Proceeding</span></span>
+<span style="position:absolute;left:44.00px;top:339.75px" class="cls_004"><span class="cls_004">Costs in Original LT Proceeding</span></span>
 <span style="position:absolute;left:192.00px;top:339.55px" class="cls_003"><span class="cls_003">$_</span>'. $_POST['costs_original_lt_proceeding'] .'</span>
-<span style="position:absolute;left:290.68px;top:339.55px" class="cls_003"><span class="cls_003">_</span></span>
-<span style="position:absolute;left:84.00px;top:351.75px" class="cls_004"><span class="cls_004">Costs in this Proceeding</span></span>
+<span style="position:absolute;left:74.00px;top:351.75px" class="cls_004"><span class="cls_004">Costs in this Proceeding</span></span>
 <span style="position:absolute;left:192.00px;top:351.55px" class="cls_003"><span class="cls_003">$_</span>'. $_POST['costs_this_proceeding'] .'</span>
-<span style="position:absolute;left:290.68px;top:351.55px" class="cls_003"><span class="cls_003">_</span></span>
-<span style="position:absolute;left:124.85px;top:364.05px" class="cls_004"><span class="cls_004">Attorney Fees</span></span>
+<span style="position:absolute;left:114.85px;top:364.05px" class="cls_004"><span class="cls_004">Attorney Fees</span></span>
 <span style="position:absolute;left:192.00px;top:363.55px" class="cls_003"><span class="cls_003">$_</span>'. $_POST['attorney_fees'] .'</span>
-<span style="position:absolute;left:290.68px;top:363.55px" class="cls_003"><span class="cls_003">_</span></span>
-<span style="position:absolute;left:162.00px;top:393.75px" class="cls_006"><span class="cls_006">Total</span></span>
+<span style="position:absolute;left:152.00px;top:393.75px" class="cls_004"><span class="cls_004">Total</span></span>
 <span style="position:absolute;left:192.00px;top:393.55px" class="cls_003"><span class="cls_003">$_</span>'. $totalFees .'</span>
-<span style="position:absolute;left:290.68px;top:393.55px" class="cls_003"><span class="cls_003">_</span></span>
-<span style="position:absolute;left:36.00px;top:423.75px" class="cls_004"><span class="cls_004">TO THE MAGISTERIAL DISTRICT JUDGE:</span></span>
-<span style="position:absolute;left:36.00px;top:435.00px" class="cls_004"><span class="cls_004">The Plaintiff(s) named below, having obtained a judgment for possession of real property located at:</span>'.$defendanthouseNum.' '.$defendantStreetName.' '. $_POST['unit_number'] . '<br>'.$defendantTown .',' . $defendantState.' '.$defendantZipcode.'  </p></span>
-<span style="position:absolute;left:36.00px;top:493.25px" class="cls_004"><span class="cls_004">Address if any:</span></span>
-<span style="position:absolute;left:36.00px;top:544.14px" class="cls_004"><span class="cls_004">Requests that you issue an ORDER FOR POSSESSION for such property.</span></span>
-<span style="position:absolute;left:36.40px;top:565.11px" class="cls_004"><span class="cls_004">I certify that this filing complies with the provisions of the Case Records Public Access Policy of the Unified Judicial</span></span>
-<span style="position:absolute;left:36.40px;top:575.91px" class="cls_004"><span class="cls_004">System of Pennsylvania that require filing confidential information and documents differently than non-confidential</span></span>
-<span style="position:absolute;left:36.40px;top:586.71px" class="cls_004"><span class="cls_004">information and documents.</span></span>
-<span style="position:absolute;left:36.70px;top:632.55px" class="cls_004"><span class="cls_004">Plaintiff:</span> '. $plantiffName .'</span>
-<span style="position:absolute;left:400.00px;top:802.40px" class="cls_004"><span class="cls_004">Date:</span> '. date("d/m/Y") .'</span>
-<span style="position:absolute;left:420.00px;top:855.55px" class="cls_004"><span class="cls_004">Plaintiff Signature:</span><img style="position:absolute; top:-60px" width="160" height="65" src="'.$_POST['signature_source'].'"/></span>
-<span style="position:absolute;left:55.40px;top:985.85px" class="cls_007"><span class="cls_007">AOPC 311A</span></span>
-<span style="position:absolute;left:505.75px;top:865.50px" class="cls_008"><span class="cls_008">FREE INTERPRETER</span></span>
-<span style="position:absolute;left:505.75px;top:875.50px" class="cls_009"><span class="cls_009">www.pacourts.us/language-rights</span></span><br>
-<span style = "position:absolute;left:303.75px;top:865.50px" class="cls_008" ><span class="cls_008" > CourtZip ID # </span ></span ><br >
+<span style="position:absolute;left:36.00px;top:523.75px" class="cls_004"><span class="cls_004">TO THE MAGISTERIAL DISTRICT JUDGE:</span></span>
+<span style="position:absolute;left:36.00px;top:535.00px" class="cls_004"><span class="cls_004">The Plaintiff(s) named below, having obtained a judgment for possession of real property located at:</span>'.$defendanthouseNum.' '.$defendantStreetName.' '. $_POST['unit_number'] . '<br>'.$defendantTown .',' . $defendantState.' '.$defendantZipcode.'  </p></span>
+<span style="position:absolute;left:36.00px;top:593.25px" class="cls_004"><span class="cls_004">Address if any:</span></span>
+<span style="position:absolute;left:36.00px;top:644.14px" class="cls_004"><span class="cls_004">Requests that you issue an ORDER FOR POSSESSION for such property.</span></span>
+<span style="position:absolute;left:36.40px;top:665.11px" class="cls_004"><span class="cls_004">I certify that this filing complies with the provisions of the Case Records Public Access Policy of the Unified Judicial</span></span>
+<span style="position:absolute;left:36.40px;top:675.91px" class="cls_004"><span class="cls_004">System of Pennsylvania that require filing confidential information and documents differently than non-confidential</span></span>
+<span style="position:absolute;left:36.40px;top:686.71px" class="cls_004"><span class="cls_004">information and documents.</span></span>
+<span style="position:absolute;left:36.70px;top:702.40px" class="cls_004"><span class="cls_004">Plaintiff:</span><span style="text-decoration:underline;" '. $plantiffName .'</span></span>
+<span style="position:absolute;left:415.00px;top:702.40px" class="cls_004"><span class="cls_004">Date:</span> '. date("d/m/Y") .'</span>
+<span style="position:absolute;left:415.00px;top:735.55px" class="cls_004"><span class="cls_004">Plaintiff Signature:</span><img style="position:absolute; top:-60px" width="160" height="65" src="'.$_POST['signature_source'].'"/></span>
+<span style="position:absolute;left:55.40px;top:860.50px" class="cls_007"><span class="cls_007">AOPC 311A</span></span>
+<span style="position:absolute;left:525.75px;top:860.50px" class="cls_008"><span class="cls_008">FREE INTERPRETER</span></span>
+<span style="position:absolute;left:525.75px;top:875.50px" class="cls_009"><span class="cls_009">www.pacourts.us/language-rights</span></span><br>
+<span style = "position:absolute;left:293.75px;top:860.50px" class="cls_008" ><span class="cls_008" > CourtZip ID # </span ></span ><br >
 </span></body></html>
 ');
 
