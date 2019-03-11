a31f4f08d780866feff7dd30036adcfbf36a4af7
Andrew Gaidis <agaidis@moneymappress.com>
2019-03-11 11:55:51 -0400

    small fixes

--- resources/views/aboutUs.blade.php
+++ resources/views/aboutUs.blade.php
@@ -13,7 +13,7 @@
 
                         <div>We hope you find CourtZip to be helpful. If you have an idea for how to make it better, or are interested in partnering with CourtZip , please email <A HREF="mailto:info@courtzip.com">info@courtzip.com</A>.</div> <br /> <br />
 
-                        <div>CourtZip has a long way to go to achieve it's long-term ambitions, and we hope you can be a part of the process as we set-off to use technology to make the local court filing process simpler, straight-forward, and quicker for everyone involved.</div> <br /> <br />
+                        <div>CourtZip has a long way to go to achieve it's long-term ambitions, and we hope you can be a part of the process as we set-off to use technology to make the local court filing process simpler, straight-forward, and quicker for everyone involved.</div> <br /><br />
 
                         <div>Thanks for your interest in CourtZip!</div>
 
