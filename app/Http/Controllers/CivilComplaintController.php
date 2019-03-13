c4ee5c0ffac2b2ce5041467e007a9fe5c8430ac0
Andrew Gaidis <agaidis@moneymappress.com>
2019-03-13 16:07:46 -0400

    committing fixes

--- app/Http/Controllers/CivilComplaintController.php
+++ app/Http/Controllers/CivilComplaintController.php
@@ -132,7 +132,7 @@ span.cls_003{font-family:Arial,serif;font-size:14px;color:rgb(0,0,0);font-weight
 span.cls_004{font-family:Arial,serif;font-size:13px;color:rgb(0,0,0);font-weight:normal;font-style:normal;text-decoration: none}
 span.cls_005{font-family:Arial,serif;font-size:10px;color:rgb(0,0,0);font-weight:normal;font-style:normal;text-decoration: none}
 span.cls_006{font-family:Arial,serif;font-size:8px;color:rgb(0,0,0);font-weight:normal;font-style:normal;text-decoration: none}
-span.cls_007{font-family:Arial,serif;font-size:12px;color:rgb(0,0,0);font-weight:bold;font-style:normal;text-decoration: none}
+span.cls_007{font-family:Arial,serif;font-size:13px;color:rgb(0,0,0);font-weight:bold;font-style:normal;text-decoration: none}
 span.cls_008{font-family:Arial,serif;font-size:10px;color:rgb(0,0,0);font-weight:normal;font-style:normal;text-decoration: none}
 span.cls_009{font-family:Arial,serif;font-size:10px;color:rgb(0,0,0);font-weight:bold;font-style:normal;text-decoration: none}
 span.cls_010{font-family:Arial,serif;font-size:10px;color:rgb(0,0,0);font-weight:normal;font-style:normal;text-decoration: none}
@@ -169,22 +169,22 @@ span.cls_011{font-family:Arial,serif;font-size:10px;color:rgb(0,0,0);font-weight
 <span style="position:absolute;left:55px;top:390px" class="cls_003"><span class="cls_003">Pa.R.C.P.M.D.J. No. 206 sets forth those costs recoverable by the prevailing party.</span></span>
 <span style="position:absolute;left:55px;top:405px" class="cls_003"><span class="cls_003">To The Defendant:  The above named plaintiff(s) asks judgment against you for $__________________ together with costs</span></span>
 <span style="position:absolute;left:166px;top:419px" class="cls_003"><span class="cls_003">upon the following claim (Civil fines must include citation of the statute or ordinance violated):</span></span>
-<span style="position:absolute;left:55px;top:720px" class="cls_003"><span class="cls_003">I, ____________'.$_POST['tenant_name'].'______________ verify that the facts set forth in this complaint are true and correct to the</span></span>
-<span style="position:absolute;left:55px;top:737px" class="cls_003"><span class="cls_003">best of my knowledge, information, and belief.  This statement is made subject to the penalties of Section 4904 of the</span></span>
-<span style="position:absolute;left:55px;top:750px" class="cls_003"><span class="cls_003">Crimes Code (18 PA. C.S. § 4904) related to unsworn falsification to authorities.</span></span>
-<span style="position:absolute;left:55px;top:777px" class="cls_003"><span class="cls_003">I certify that this filing complies with the provisions of the Case Records Public Access Policy of the Unified Judicial System</span></span>
-<span style="position:absolute;left:55px;top:794px" class="cls_003"><span class="cls_003">of Pennsylvania that require filing confidential information and documents differently than non-confidential information and</span></span>
-<span style="position:absolute;left:55px;top:812px" class="cls_003"><span class="cls_003">documents.</span></span>
+<span style="position:absolute;left:55px;top:690px" class="cls_003"><span class="cls_003">I, ____________'.$_POST['tenant_name'].'______________ verify that the facts set forth in this complaint are true and correct to the</span></span>
+<span style="position:absolute;left:55px;top:705px" class="cls_003"><span class="cls_003">best of my knowledge, information, and belief.  This statement is made subject to the penalties of Section 4904 of the</span></span>
+<span style="position:absolute;left:55px;top:720px" class="cls_003"><span class="cls_003">Crimes Code (18 PA. C.S. § 4904) related to unsworn falsification to authorities.</span></span>
+<span style="position:absolute;left:55px;top:750px" class="cls_003"><span class="cls_003">I certify that this filing complies with the provisions of the Case Records Public Access Policy of the Unified Judicial System</span></span>
+<span style="position:absolute;left:55px;top:775px" class="cls_003"><span class="cls_003">of Pennsylvania that require filing confidential information and documents differently than non-confidential information and</span></span>
+<span style="position:absolute;left:55px;top:790px" class="cls_003"><span class="cls_003">documents.</span></span>
 <span style="position:absolute;left:480px;top:780px" class="cls_004"><img style="position:absolute; top:-60px" width="160" height="65" src="'.$_POST['signature_source'].'"/><span class="cls_004">(Signature of Plaintiff or Authorized Agent)</span></span>
 <span style="position:absolute;left:55px;top:px" class="cls_004"><span class="cls_004">The plaintiff\'s attorney shall file an entry of appearance with the magisterial district court pursuant to Pa.R.C.P.M.D.J. 207.1</span></span>
-<span style="position:absolute;left:55px;top:850px" class="cls_007"><span class="cls_007">If you intend to enter a defense to this complaint, you should notify this office immediately at the above telephone number.  You</span></span>
-<span style="position:absolute;left:55px;top:865px" class="cls_007"><span class="cls_007">must appear at the hearing and present your defense.  Unless you do, judgment may be entered against you by default.</span></span>
-<span style="position:absolute;left:55px;top:880px" class="cls_004"><span class="cls_004">If you have a claim against the plaintiff which is within the magisterial district judge jurisdiction and which you intend to assert at the</span></span>
-<span style="position:absolute;left:55px;top:895px" class="cls_004"><span class="cls_004">hearing, you must file it on a complaint form at this office at least five days before the date set for the hearing.</span></span>
-<span style="position:absolute;left:55px;top:910px" class="cls_007"><span class="cls_007">If you are disabled and require a reasonable accommodation to gain access to the Magisterial District Court and its services,</span></span>
-<span style="position:absolute;left:55px;top:925px" class="cls_007"><span class="cls_007">please contact the Magisterial District Court at the above address or telephone number.  We are unable to provide</span></span>
-<span style="position:absolute;left:55px;top:940px" class="cls_007"><span class="cls_007">transportation.</span></span>
-<span style="position:absolute;left:55px;top:985px" class="cls_008"><span class="cls_008">AOPC 308A</span></span>
+<span style="position:absolute;left:50px;top:850px" class="cls_007"><span class="cls_007">If you intend to enter a defense to this complaint, you should notify this office immediately at the above telephone number.  You</span></span>
+<span style="position:absolute;left:50px;top:865px" class="cls_007"><span class="cls_007">must appear at the hearing and present your defense.  Unless you do, judgment may be entered against you by default.</span></span>
+<span style="position:absolute;left:50px;top:895px" class="cls_004"><span class="cls_004">If you have a claim against the plaintiff which is within the magisterial district judge jurisdiction and which you intend to assert at the</span></span>
+<span style="position:absolute;left:50px;top:910px" class="cls_004"><span class="cls_004">hearing, you must file it on a complaint form at this office at least five days before the date set for the hearing.</span></span>
+<span style="position:absolute;left:50px;top:925px" class="cls_007"><span class="cls_007">If you are disabled and require a reasonable accommodation to gain access to the Magisterial District Court and its services,</span></span>
+<span style="position:absolute;left:50px;top:940px" class="cls_007"><span class="cls_007">please contact the Magisterial District Court at the above address or telephone number.  We are unable to provide</span></span>
+<span style="position:absolute;left:50px;top:940px" class="cls_007"><span class="cls_007">transportation.</span></span>
+<span style="position:absolute;left:50px;top:985px" class="cls_008"><span class="cls_008">AOPC 308A</span></span>
 <span style="position:absolute;left:605px;top:985px" class="cls_009"><span class="cls_009">FREE INTERPRETER</span></span>
 <span style="position:absolute;left:590px;top:1000px" class="cls_011"><span class="cls_011"> </span><A HREF="http://www.pacourts.us/language-rights/">www.pacourts.us/language-rights</A> </span>
 <span style = "position:absolute;left:270px;top:985px" class="cls_008" ><span class="cls_008" > CourtZip ID #'.$evictionId.' </span ></span ><br >
