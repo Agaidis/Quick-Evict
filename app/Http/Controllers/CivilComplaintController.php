2de2da9e97642ef4c510129e480e287359bea17c
Andrew Gaidis <agaidis@moneymappress.com>
2019-03-13 16:15:37 -0400

    committing fixes

--- app/Http/Controllers/CivilComplaintController.php
+++ app/Http/Controllers/CivilComplaintController.php
@@ -169,7 +169,7 @@ span.cls_011{font-family:Arial,serif;font-size:10px;color:rgb(0,0,0);font-weight
 <span style="position:absolute;left:55px;top:388px" class="cls_003"><span class="cls_003">Pa.R.C.P.M.D.J. No. 206 sets forth those costs recoverable by the prevailing party.</span></span>
 <span style="position:absolute;left:55px;top:410px" class="cls_003"><span class="cls_003">To The Defendant:  The above named plaintiff(s) asks judgment against you for $__________________ together with costs</span></span>
 <span style="position:absolute;left:166px;top:425px" class="cls_003"><span class="cls_003">upon the following claim (Civil fines must include citation of the statute or ordinance violated):</span></span>
-<span style="position:absolute;left:90px;top:590px" class="cls_003">'.$_POST['tenant_name'].'</span>
+<span style="position:absolute;left:155px;top:590px" class="cls_003">'.$_POST['tenant_name'].'</span>
 <span style="position:absolute;left:55px;top:600px" class="cls_003"><span class="cls_003">I, ________________________________ verify that the facts set forth in this complaint are true and correct to the</span></span>
 <span style="position:absolute;left:55px;top:615px" class="cls_003"><span class="cls_003">best of my knowledge, information, and belief.  This statement is made subject to the penalties of Section 4904 of the</span></span>
 <span style="position:absolute;left:55px;top:645px" class="cls_003"><span class="cls_003">Crimes Code (18 PA. C.S. § 4904) related to unsworn falsification to authorities.</span></span>
