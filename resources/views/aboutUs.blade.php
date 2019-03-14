b1985d2d2d1d00d68e3edbaad245c14f1a9547fe
Andrew Gaidis <agaidis@moneymappress.com>
2019-03-14 15:39:06 -0400

    changing text

--- resources/views/aboutUs.blade.php
+++ resources/views/aboutUs.blade.php
@@ -9,15 +9,14 @@
                 <div class="card-header"><h2>About Us</h2></div>
                 <div class="card-body">
                     <div class="row">
-                        <div>Local civil and eviction filings can be incredibly frustrating, time-consuming, and confusing. CourtZip was created and is run by a team that has a deep background in technology, engineering, and legal process. We understood how difficult the civil and eviction process can be at a local level. We heard complaints of civil filings not filed properly, rejected by judges for manual errors in the process, or just was too complicated to understand. We took our engineering background and built out a better solution -- to make the process easier to everyone involved.</div><br /> <br />
+                        <div>Civil and Landlord-Tenant Complaints can be incredibly frustrating, time-consuming, and confusing. CourtZip was created and is run by a team that has a deep background in technology, engineering, and legal process. We understood how difficult the civil process can be at the Magisterial District Court level. We heard complaints of civil filings not filed properly, rejected by judges for manual errors in the process, or just was too complicated to understand. We took our engineering background and built a better solution -- to make the process easier for everyone involved.</div>
 
-                        <div>We hope you find CourtZip to be helpful. If you have an idea for how to make it better, or are interested in partnering with CourtZip , please email <A HREF="mailto:info@courtzip.com">info@courtzip.com</A>.</div> <br /> <br />
+                        <div>We hope you find CourtZip helpful. If you have an idea for how to make it better, or are interested in partnering with CourtZip, please email&nbsp;<u><a href="mailto:info@courtzip.com">info@courtzip.com</a></u>.</div>
 
-                        <div>CourtZip has a long way to go to achieve it's long-term ambitions, and we hope you can be a part of the process as we set-off to use technology to make the local court filing process simpler, straight-forward, and quicker for everyone involved.</div> <br /><br />
+                        <div>CourtZip has a long way to go to achieve its long-term ambitions, and we hope you can be a part of the process as we set-off to use technology to make the District Court filing process simpler, straight-forward, and quicker for everyone involved.</div>
 
                         <div>Thanks for your interest in CourtZip!</div>
-
-                        <br /><div>- CourtZip Team</div>
+                        <div>- CourtZip Team</div>
                     </div>
                </div>
             </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,24 +21,21 @@
                 <div class="card">
                     <div class="card-body">
                         <h2 class="titles">Welcome to Court<em>Zip</em>!</h2>
-                        <div class="home_text">CourtZip is the leader in combining technology and court filing expertise to improve the process for filing complaints at local courts and magistrates. CourtZip makes it easy to file local complaints including civil complaints under $10,000, landlord-tenant complaints (for evictions), and order of possession (for possession of property from eviction).</div>
-                        <br />
-                        <div class="home_text">Prior to CourtZip, the filing process was dramatically outdated. Filing at the local court caused a lot of extra manual work and delays for everyone involved. CourtZip brought together a collaboration between cutting-edge judges, government officials, property managers, real estate owners, and web developers to create a better solution.</div>
-                        <br />
-                        <div>CourtZip solves the following problems:</div><br />
-                        <div>
-                            <ul>
-                                <li><strong>Geo-Coded Local Court Zones</strong>: Local civil complaints must be filed in the correct court, which is often confusing. CourtZip has geo-coded each address into the specific court to be filed. The owner or property manager just types in the address and CourtZip matches the address to the proper court --- never have to call the court again or file in the wrong place!<br /><br /></li>
-                                <li><strong>Filing Fees</strong>: Each court has different filing fees based on a # of different variables. CourtZip built a database of every fee for each court so you never have to call a court again to know the fee. Just type in the basic filing info and CourtZip pulls the specific filing fee for you, saving time for everyone!<br /><br /></li>
-                                <li><strong>Civil Complaint</strong>: Whether you are filing one civil complaint or file hundreds each month, CourtZip can make it easier and faster for you to file.<br /><br /></li>
-                                <li><strong>Landlord-Tenant Complaint</strong>: CourtZip makes the Landlord-Tenant complaint easier to fill out correctly by walking you through the process with easy to understand fields to fill out. Once filled in, a full landlord-tenant complaint is accurately completed by CourtZip in the proper form needed by the state of Pennsylvania. This results in less errors in incorrect Civil, Landlord-Tenant, and Order of Possession complaint forms. Never have a mistake from mis-read handwriting again!<br /><br /></li>
-                                <li><strong>Online Filing</strong>: Evictions and local civil complaints are currently filed via the mail, which causes huge delays in filing. CourtZip enables you to file immediately online -- dramatically decreasing any delays. Each judge has their own CourtZip dashboard where they see the filing the same day it was submitted and set-up a court date immediately. Online filing also decreases the delays from incorrect filings via mail being sent back and forth.<br /><br /></li>
-                                <li><strong>Easy Online Payment</strong>: CourtZip accepts debit or credit cards to make it easy for anyone to pay for the filing fee online.</li>
-                            </ul>
+                        <div>CourtZip is the leader in combining technology and court filing expertise to improve the process for filing Complaints in Pennsylvania Magisterial District Courts. CourtZip makes it easy to file Civil Complaints under $12,000, Landlord-Tenant Complaints (for evictions), and Orders of Possession (for possession of property from eviction).</div>
 
-                            <div>CourtZip is constantly growing -- click here to see where CourtZip is live: <a href="https://courtzip.com/where-does-this-work" target="_blank" data-saferedirecturl="https://www.google.com/url?q=https://courtzip.com/where-does-this-work&amp;source=gmail&amp;ust=1552399282868000&amp;usg=AFQjCNF-m5XENIdqZcAXtJzQ63df9HROHg">Locations</a></div>
-                            <div>To get more info --- contact CourtZip here: <a href="mailto:Info@courtzip.com" target="_blank">Info@courtzip.com</a></div>
-                        </div>
+                        <div>Prior to CourtZip, the filing process was outdated. Filing in the District Court required extra manual work and delays for everyone involved. CourtZip results from a collaboration of innovative judges, attorneys, government officials, property managers, real estate owners, and web developers to create a better solution.</div>
+
+                        <div>CourtZip solves the following problems:</div>
+
+                        <ul>
+                            <li><strong>Which Court to File My Complaint: </strong>Civil Complaints (under $12,000 at issue) &nbsp;and Landlord-Tenant Complaints must be filed in the correct Magisterial District Court, which is often confusing as most counties have numerous Magisterial District Courts with irregular boundaries. CourtZip has geo-coded each Pennsylvania address into the specific Court to be filed. The user types in the address and CourtZip identifies the proper court.&nbsp; You never again have to call different Courts, search cumbersome websites, or file in the wrong Court.</li>
+                            <li><strong>How Much is Filing Fee:</strong> Each Court has different filing fees based on different variables, which generally require a call to the proper Court to determine. CourtZip built a database, updated routinely, of every fee for each Court. You never have to call a Court again to ascertain the fees. Just type in the basic filing info and CourtZip populates the specific filing fee for you, saving time for the Court staff and filers.</li>
+                            <li><strong>Creating a Typed Civil Complaint, Landlord-Tenant Complaint, or Order of </strong><strong>Possession:</strong> Whether you are filing one complaint or you file hundreds each month, CourtZip can make it easier and faster for you to draft them.</li>
+                            <li><strong>Faster Delivery of Filing: </strong>Landlord-Tenant, Order of Possession, and Civil Complaints are currently filed by mail (or in person), which causes delays and inaccuracies in filing. CourtZip enables you to file immediately online, eliminating delays and errors. Each Court has its own CourtZip dashboard where its staff and assigned Magisterial District Judge can access the filing and can set-up a hearing date.</li>
+                            <li><strong>Easier Payment</strong>: CourtZip accepts debit or credit cards to make it easy for anyone to pay for the filing fee.</li>
+                        </ul>
+                        <p>CourtZip is constantly growing -- click here to see where CourtZip is live:&nbsp;<u><a href="https://courtzip.com/where-does-this-work" data-saferedirecturl="https://www.google.com/url?q=https://courtzip.com/where-does-this-work&amp;source=gmail&amp;ust=1552678182363000&amp;usg=AFQjCNEzs8xrrhkzKG6qWWWw7u29nIRo9g">Locations</a></u></p>
+                        <p>To get more info --- contact CourtZip here:&nbsp;<u><a href="mailto:Info@courtzip.com">Info@courtzip.com</a></u></p>
                     </div>
                 </div>
             </div>
