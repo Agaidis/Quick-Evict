1d81512811bc4b40e6aaa5c8db81ad58eaa55c54
Andrew Gaidis <andrewgaidis@Andrews-MacBook-Pro.local>
2019-03-29 08:31:57 -0400

    arranging order of dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -36,7 +36,7 @@ class DashboardController extends Controller
             $courtNumber = Auth::user()->court_id;
 
 
-            $evictions = DB::select('select * from evictions id DESC');
+            $evictions = DB::select('select * from evictions');
 //            if (Auth::user()->role == 'Administrator') {
 //                $evictions = DB::select('select * from evictions ORDER BY FIELD(status, "Created LTC", "LTC Mailed", "LTC Submitted Online", "Court Hearing Scheduled", "Court Hearing Extended", "Judgement Issued in Favor of Owner", "Judgement Denied by Court", "Tenant Filed Appeal", "OOP Mailed", "OOP Submitted Online", "Paid Judgement", "Locked Out Tenant"), id DESC');
 //            } else if (Auth::user()->role == 'General User') {
