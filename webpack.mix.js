04e00b2925b37a1749b8c40b587d4f37897e4fbe
Andrew Gaidis <andrewgaidis@andrews-mbp.home>
2019-04-20 18:04:21 -0400

    Another attempt at versioning javascript

--- webpack.mix.js
+++ webpack.mix.js
@@ -29,7 +29,6 @@ let mix = require('laravel-mix');
 //mix.js('resources/assets/js/app.js', 'public/js')
     //.sass('resources/assets/sass/app.scss', 'public/css')
     .styles([
-        'public/css/app.css',
         'resources/assets/css/bootstrap-timepicker.min.css',
         'resources/assets/css/dashboard.css',
         'resources/assets/css/datatables.min.css',
