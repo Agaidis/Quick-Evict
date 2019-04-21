e3880ca85275255bb2bed6529fe4e27b555f888d
Andrew Gaidis <andrewgaidis@andrews-mbp.home>
2019-04-20 20:49:12 -0400

    Another attempt at versioning javascript

--- resources/views/civilComplaint.blade.php
+++ resources/views/civilComplaint.blade.php
@@ -130,7 +130,7 @@
                                             </div>
                                             <div class="modal-body">
                                                 <div class="row">
-                                                    <div class="col-md-offset-3 col-sm-8">
+                                                    <div>
                                                         <span><input type="checkbox" id="legal_checkbox"/><em><label for="legal_checkbox">By checking this box, I understand that my electronic signature constitutes a legal signature, <br>and that by entering my name above I acknowledge and warrant the accuracy of the information provided in this document.</label></em></span>
                                                         <span style="font-size: 32px;">X</span> <canvas id="signature_pad" width="600" height="200" style="touch-action: none;"></canvas>
                                                         <div class="signature_pad_footer">
