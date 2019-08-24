c58ef4ec8dbf5d52e4978dca56cd34d53c9967cb
Andrew Gaidis <andrewgaidis@Andrews-MacBook-Pro.local>
2019-08-24 17:21:15 -0400

    committing mailgun functionality

--- app/Classes/Mailer.php
+++ app/Classes/Mailer.php
@@ -11,7 +11,7 @@ class Mailer
     public function sendMail($mailTo, $subject, $message) {
 
         // First, instantiate the SDK with your API credentials
-        $mg = Mailgun::create('1f6b8c342bbe30ae8a7e9afbeebe0be0-3939b93a-12b78a07'); // For US servers
+        $mg = Mailgun::create(env('MAIL_API_KEY')); // For US servers
 
         $mg->messages()->send('mg.courtzip.com', [
             'from'    => 'Court Zip Administration <mailgun@mg.courtzip.com>',
