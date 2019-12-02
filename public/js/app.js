00be5357fee0c637e42df78f3358da13ed64f9af
Andrew Gaidis <andrewgaidis@Andrews-MacBook-Pro.local>
2019-12-02 14:07:59 -0500

    changing the stripToken to other form

--- public/js/app.js
+++ public/js/app.js
@@ -49401,119 +49401,116 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-var canvas = document.querySelector("canvas");
-var signaturePad = new SignaturePad(canvas, {}); //Clear button to remove signature drawing
+if (document.location.href.split('/')[3] === 'new-file') {
+  // Submit the form with the token ID.
+  var stripeTokenHandler = function stripeTokenHandler(token) {// Submit the form
+    // form.submit();
+  };
 
-$('.clear_signature').on('click', function () {
-  // Clears the canvas
-  signaturePad.clear();
-}); // Create a Stripe client.
+  var canvas = document.querySelector("canvas");
+  var signaturePad = new SignaturePad(canvas, {}); //Clear button to remove signature drawing
 
-var stripe = Stripe('pk_test_FTcQeimeSasisJpDTYgHEMTh'); // Create an instance of Elements.
+  $('.clear_signature').on('click', function () {
+    // Clears the canvas
+    signaturePad.clear();
+  }); // Create a Stripe client.
 
-var elements = stripe.elements(); // Custom styling can be passed to options when creating an Element.
-// (Note that this demo uses a wider set of styles than the guide below.)
+  var stripe = Stripe('pk_test_FTcQeimeSasisJpDTYgHEMTh'); // Create an instance of Elements.
 
-var style = {
-  base: {
-    color: '#32325d',
-    fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-    fontSmoothing: 'antialiased',
-    fontSize: '16px',
-    '::placeholder': {
-      color: '#aab7c4'
+  var elements = stripe.elements(); // Custom styling can be passed to options when creating an Element.
+  // (Note that this demo uses a wider set of styles than the guide below.)
+
+  var style = {
+    base: {
+      color: '#32325d',
+      fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+      fontSmoothing: 'antialiased',
+      fontSize: '16px',
+      '::placeholder': {
+        color: '#aab7c4'
+      }
+    },
+    invalid: {
+      color: '#fa755a',
+      iconColor: '#fa755a'
     }
-  },
-  invalid: {
-    color: '#fa755a',
-    iconColor: '#fa755a'
-  }
-}; // Create an instance of the card Element.
+  }; // Create an instance of the card Element.
 
-var card = elements.create('card', {
-  style: style
-}); // Add an instance of the card Element into the `card-element` <div>.
+  var card = elements.create('card', {
+    style: style
+  }); // Add an instance of the card Element into the `card-element` <div>.
 
-card.mount('#card-element'); // Handle real-time validation errors from the card Element.
+  card.mount('#card-element'); // Handle real-time validation errors from the card Element.
 
-card.addEventListener('change', function (event) {
-  var displayError = document.getElementById('card-errors');
+  card.addEventListener('change', function (event) {
+    var displayError = document.getElementById('card-errors');
 
-  if (event.error) {
-    displayError.textContent = event.error.message;
-  } else {
-    displayError.textContent = '';
-  }
-}); // Handle form submission.
-
-var form = document.getElementById('pay_sign_submit');
-form.addEventListener('click', function (event) {
-  console.log('made it in here');
-  stripe.createToken(card).then(function (result) {
-    if (result.error) {
-      // Inform the user if there was an error.
-      var errorElement = document.getElementById('card-errors');
-      errorElement.textContent = result.error.message;
+    if (event.error) {
+      displayError.textContent = event.error.message;
     } else {
-      // Send the token to your server.
-      console.log(result.token);
-      stripeTokenHandler(result.token);
+      displayError.textContent = '';
+    }
+  }); // Handle form submission.
+
+  var form = document.getElementById('pay_sign_submit');
+  form.addEventListener('click', function (event) {
+    console.log('made it in here');
+    stripe.createToken(card).then(function (result) {
+      if (result.error) {
+        // Inform the user if there was an error.
+        var errorElement = document.getElementById('card-errors');
+        errorElement.textContent = result.error.message;
+      } else {
+        // Send the token to your server.
+        console.log(result.token.id);
+        $('stripe_token').val(result.token.id);
+      }
+    });
+    var url = '';
+
+    if ($('#file_type').val() === 'oop') {
+      url = 'new-oop/pdf-data';
+    } else if ($('#file_type').val() === 'ltc') {
+      url = 'new-ltc/pdf-data';
+    } else if ($('#file_type').val() === 'civil') {
+      url = 'new-civil-complaint/pdf-data';
+    } else {
+      alert('Error with finding File Type. Contact Support');
+    }
+
+    if ($('#legal_checkbox').is(':checked')) {
+      console.log($('#eviction_form').serialize());
+      $('#modal_signature').modal('toggle');
+      var $body = $("body");
+      $body.addClass("loading");
+      var dataURL = signaturePad.toDataURL(); // save image as PNG
+
+      $('#signature_source').val(dataURL);
+      var formData = $('#eviction_form').serialize();
+      $.ajaxSetup({
+        headers: {
+          'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+      });
+      $.ajax({
+        beforeSend: function beforeSend(xhr) {
+          xhr.setRequestHeader('X-CSRF-TOKEN', $("#token").attr('content'));
+        },
+        url: url,
+        type: 'POST',
+        data: formData,
+        success: function success(data) {
+          console.log(data);
+          window.location.href = environmentPath + '/dashboard';
+        },
+        error: function error(data) {
+          console.log(data);
+        }
+      });
+    } else {
+      alert('You need to check the Signature checkbox above to agree to the digital terms in order to continue.');
     }
   });
-  var url = '';
-
-  if ($('#file_type').val() === 'oop') {
-    url = 'new-oop/pdf-data';
-  } else if ($('#file_type').val() === 'ltc') {
-    url = 'new-ltc/pdf-data';
-  } else if ($('#file_type').val() === 'civil') {
-    url = 'new-civil-complaint/pdf-data';
-  } else {
-    alert('Error with finding File Type. Contact Support');
-  }
-
-  if ($('#legal_checkbox').is(':checked')) {
-    $('#modal_signature').modal('toggle');
-    var $body = $("body");
-    $body.addClass("loading");
-    var dataURL = signaturePad.toDataURL(); // save image as PNG
-
-    $('#signature_source').val(dataURL);
-    var formData = $('#eviction_form').serialize();
-    $.ajaxSetup({
-      headers: {
-        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-      }
-    });
-    $.ajax({
-      beforeSend: function beforeSend(xhr) {
-        xhr.setRequestHeader('X-CSRF-TOKEN', $("#token").attr('content'));
-      },
-      url: url,
-      type: 'POST',
-      data: formData,
-      success: function success(data) {
-        console.log(data);
-        window.location.href = environmentPath + '/dashboard';
-      },
-      error: function error(data) {
-        console.log(data);
-      }
-    });
-  } else {
-    alert('You need to check the Signature checkbox above to agree to the digital terms in order to continue.');
-  }
-}); // Submit the form with the token ID.
-
-function stripeTokenHandler(token) {
-  // Insert the token ID into the form so it gets submitted to the server
-  var form = document.getElementById('eviction_form');
-  var hiddenInput = document.createElement('input');
-  hiddenInput.setAttribute('type', 'hidden');
-  hiddenInput.setAttribute('name', 'stripeToken');
-  hiddenInput.setAttribute('value', token.id);
-  form.appendChild(hiddenInput); // Submit the form
-  // form.submit();
 }
 
 /***/ }),
--- resources/assets/js/stripe.js
+++ resources/assets/js/stripe.js
@@ -1,123 +1,124 @@
-let canvas = document.querySelector("canvas");
-let signaturePad = new SignaturePad(canvas, {
-
-});
+if (document.location.href.split('/')[3] === 'new-file') {
+    let canvas = document.querySelector("canvas");
+    let signaturePad = new SignaturePad(canvas, {});
 
 //Clear button to remove signature drawing
-$('.clear_signature').on('click', function() {
-    // Clears the canvas
-    signaturePad.clear();
-});
+    $('.clear_signature').on('click', function () {
+        // Clears the canvas
+        signaturePad.clear();
+    });
 // Create a Stripe client.
-var stripe = Stripe('pk_test_FTcQeimeSasisJpDTYgHEMTh');
+    var stripe = Stripe('pk_test_FTcQeimeSasisJpDTYgHEMTh');
 
 // Create an instance of Elements.
-var elements = stripe.elements();
+    var elements = stripe.elements();
 
 // Custom styling can be passed to options when creating an Element.
 // (Note that this demo uses a wider set of styles than the guide below.)
-var style = {
-    base: {
-        color: '#32325d',
-        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-        fontSmoothing: 'antialiased',
-        fontSize: '16px',
-        '::placeholder': {
-            color: '#aab7c4'
+    var style = {
+        base: {
+            color: '#32325d',
+            fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+            fontSmoothing: 'antialiased',
+            fontSize: '16px',
+            '::placeholder': {
+                color: '#aab7c4'
+            }
+        },
+        invalid: {
+            color: '#fa755a',
+            iconColor: '#fa755a'
         }
-    },
-    invalid: {
-        color: '#fa755a',
-        iconColor: '#fa755a'
-    }
-};
+    };
 
 // Create an instance of the card Element.
-var card = elements.create('card', {style: style});
+    var card = elements.create('card', {style: style});
 
 // Add an instance of the card Element into the `card-element` <div>.
-card.mount('#card-element');
+    card.mount('#card-element');
 
 // Handle real-time validation errors from the card Element.
-card.addEventListener('change', function(event) {
-    var displayError = document.getElementById('card-errors');
-    if (event.error) {
-        displayError.textContent = event.error.message;
-    } else {
-        displayError.textContent = '';
-    }
-});
+    card.addEventListener('change', function (event) {
+        var displayError = document.getElementById('card-errors');
+        if (event.error) {
+            displayError.textContent = event.error.message;
+        } else {
+            displayError.textContent = '';
+        }
+    });
 
 // Handle form submission.
- let form = document.getElementById('pay_sign_submit');
- form.addEventListener('click', function(event) {
-     console.log('made it in here');
+    let form = document.getElementById('pay_sign_submit');
+    form.addEventListener('click', function (event) {
+        console.log('made it in here');
 
-     stripe.createToken(card).then(function(result) {
-         if (result.error) {
-             // Inform the user if there was an error.
-             var errorElement = document.getElementById('card-errors');
-             errorElement.textContent = result.error.message;
-         } else {
-             // Send the token to your server.
-             console.log(result.token);
-             stripeTokenHandler(result.token);
-         }
-     });
+        stripe.createToken(card).then(function (result) {
+            if (result.error) {
+                // Inform the user if there was an error.
+                let errorElement = document.getElementById('card-errors');
+                errorElement.textContent = result.error.message;
+            } else {
+                // Send the token to your server.
+                console.log(result.token.id);
+                $('stripe_token').val(result.token.id);
+            }
+        });
 
-     let url = '';
-     if ( $('#file_type').val() === 'oop' ) {
-         url = 'new-oop/pdf-data';
-     } else if ( $('#file_type').val() === 'ltc' ) {
-         url = 'new-ltc/pdf-data';
-     } else if ( $('#file_type').val() === 'civil' ) {
-         url = 'new-civil-complaint/pdf-data';
-     } else {
-         alert('Error with finding File Type. Contact Support');
-     }
-     if ($('#legal_checkbox').is(':checked')) {
-         $('#modal_signature').modal('toggle');
-         let $body = $("body");
-         $body.addClass("loading");
-         let dataURL = signaturePad.toDataURL(); // save image as PNG
-         $('#signature_source').val(dataURL);
+        let url = '';
+        if ($('#file_type').val() === 'oop') {
+            url = 'new-oop/pdf-data';
+        } else if ($('#file_type').val() === 'ltc') {
+            url = 'new-ltc/pdf-data';
+        } else if ($('#file_type').val() === 'civil') {
+            url = 'new-civil-complaint/pdf-data';
+        } else {
+            alert('Error with finding File Type. Contact Support');
+        }
+        if ($('#legal_checkbox').is(':checked')) {
+            console.log($('#eviction_form').serialize());
+            $('#modal_signature').modal('toggle');
+            let $body = $("body");
+            $body.addClass("loading");
+            let dataURL = signaturePad.toDataURL(); // save image as PNG
+            $('#signature_source').val(dataURL);
 
-         let formData = $('#eviction_form').serialize();
+            let formData = $('#eviction_form').serialize();
 
-         $.ajaxSetup({
-             headers: {
-                 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-             }
-         });
-         $.ajax({
-             beforeSend: function (xhr) {
-                 xhr.setRequestHeader('X-CSRF-TOKEN', $("#token").attr('content'));
-             },
-             url : url,
-             type : 'POST',
-             data : formData,
-             success : function(data) {
-                 console.log(data);
-                 window.location.href = environmentPath + '/dashboard';
-             },
-             error : function(data) {console.log(data)},
-         });
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+            $.ajax({
+                beforeSend: function (xhr) {
+                    xhr.setRequestHeader('X-CSRF-TOKEN', $("#token").attr('content'));
+                },
+                url: url,
+                type: 'POST',
+                data: formData,
+                success: function (data) {
+                    console.log(data);
+                    window.location.href = environmentPath + '/dashboard';
+                },
+                error: function (data) {
+                    console.log(data)
+                },
+            });
 
-     } else {
-         alert('You need to check the Signature checkbox above to agree to the digital terms in order to continue.')
-     }
- });
+        } else {
+            alert('You need to check the Signature checkbox above to agree to the digital terms in order to continue.')
+        }
+    });
 
- // Submit the form with the token ID.
- function stripeTokenHandler(token) {
-     // Insert the token ID into the form so it gets submitted to the server
-     let form = document.getElementById('eviction_form');
-     let hiddenInput = document.createElement('input');
-     hiddenInput.setAttribute('type', 'hidden');
-     hiddenInput.setAttribute('name', 'stripeToken');
-     hiddenInput.setAttribute('value', token.id);
-     form.appendChild(hiddenInput);
+    // Submit the form with the token ID.
+    function stripeTokenHandler(token) {
 
-     // Submit the form
-     // form.submit();
+
+
+
+
+
+        // Submit the form
+        // form.submit();
+    }
 }
