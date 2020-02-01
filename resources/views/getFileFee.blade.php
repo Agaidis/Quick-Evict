49b13a748eb3470bdcfe3d465baba93563ad56b8
Andrew Gaidis <andrewgaidis@andrews-mbp-2.home>
2020-01-31 21:31:06 -0500

    constable vs mail now works

--- resources/views/getFileFee.blade.php
+++ resources/views/getFileFee.blade.php
@@ -24,14 +24,14 @@
                                 <h2 class="titles" style="text-align:center;">Step 2: Fill in the Fields Below</h2>
                             <div id="get_file_fields_container">
                                 <div class="row col-sm-10">
-                                    <label class="labels" for="file_type_select">File Type: </label>
+                                    <label class="labels" for="file_type_select">File Type: </label><span style="color:red; margin-left:10%;" class="error_msgs" id="file_type_error_msg"></span>
                                         <select class="form-control" id="file_type_select" name="fileType">
                                             <option value="none">File type</option>
                                             <option value="ltc">Landlord Tenant-Complaint</option>
                                             <option value="oop">Request for Order of Possession</option>
                                             <option value="civil">Civil Complaint</option>
                                         </select>
-                                </div><span style="color:red;" class="error_msgs" id="file_type_error_msg"></span>
+                                </div>
                                 <div style="display:none;" class="send_method_container row col-sm-10">
                                     <label class="labels" for="send_method">Delivery Method: </label>
                                     <select class="form-control" id="send_method" name="sendMethod">
@@ -41,7 +41,7 @@
                                     </select>
                                 </div>
                                 <div class="row col-sm-10">
-                                    <label for="num_defendants" class="labels">Number of Defendants</label><span style="color:red;" class="error_msgs" id="num_def_error_msg"></span>
+                                    <label for="num_defendants" class="labels">Number of Defendants</label><span style="color:red; margin-left:10%;" class="error_msgs" id="num_def_error_msg"></span>
                                     <select class="form-control" id="num_defendants">
                                         <option value="none" selected disabled>Select # of Defendants</option>
                                         <option value="1">1</option>
@@ -68,9 +68,9 @@
                                 </div>
 
                                 <div class="row col-sm-10">
-                                    <label class="labels" for="total_judgment">Total Judgment</label>
+                                    <label class="labels" for="total_judgment">Total Judgment</label><span style="color:red; margin-left:10%" class="error_msgs" id="total_judgment_error_msg"></span>
                                     <input type="text" class="form-control eviction_fields" id="total_judgment" name="total_judgment" placeholder="$" value=""  maxlength="9"/>
-                                </div><span style="color:red;" class="error_msgs" id="total_judgment_error_msg"></span><br>
+                                </div>
                                 <div class="row col-md-12">
                                     <button id="calculate_file_fee" class="btn btn-primary">Calculate Filing Fee!</button><br>
                                     <div class="col-md-6">
