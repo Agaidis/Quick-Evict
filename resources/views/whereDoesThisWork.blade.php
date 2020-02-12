20d90721969cfc01eb5952aba4097cb911e1c069
Andrew Gaidis <andrewgaidis@Andrews-MacBook-Pro-2.local>
2020-02-12 09:27:56 -0500

    committing home page

--- resources/views/whereDoesThisWork.blade.php
+++ resources/views/whereDoesThisWork.blade.php
@@ -16,7 +16,7 @@
                         <p class="alert alert-danger">{{ Session::get('alert-success') }} <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a></p>
                     @endif
             </div> <!-- end .flash-message -->
-            <span id="where_does_this_work_content" style="text-align: center;"><div style="font-weight:bold">Thanks for your interest in CourtZip.</div>
+            <span id="where_does_this_work_content"><div style="font-weight:bold">Thanks for your interest in CourtZip.</div>
                 <div>The goal for CourtZip is to eventually make filing evictions, order of possessions, and civil complaints easier for everyone in all communities in the United States.</div>
                 <div>If you would like updates as more counties are rolled out, please submit your email here:</div><br>
                 <form method="post" action="{{ action('WhereDoesThisWorkController@store') }}" id="where_does_this_work_form">
