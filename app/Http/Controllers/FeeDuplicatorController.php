7272c252c2274a20eed88023290b669fc7ca337f
Andrew Gaidis <andrewgaidis@Andrews-MacBook-Pro-2.local>
2020-04-04 23:39:46 -0400

    fee duplicator is doneski

--- app/Http/Controllers/FeeDuplicatorController.php
+++ app/Http/Controllers/FeeDuplicatorController.php
@@ -49,14 +49,14 @@ class FeeDuplicatorController extends Controller
 
     public function duplicateFees(Request $request) {
         try {
-            Log::info($request->magistrates);
             $courtFees = CourtDetails::where('magistrate_id', $request->duplicated_magistrate)->first();
             $civilFees = CivilUnique::where('court_details_id', $courtFees->id)->first();
+
 
             foreach ($request->magistrates as $magistrate) {
 
                 $magistrateId = CourtDetails::where('magistrate_id', $magistrate)->value('id');
-                $updateCourtDetails = CourtDetails::where('magistrate_id', $magistrate)->update(
+                CourtDetails::where('magistrate_id', $magistrate)->update(
                     [
                         'one_defendant_up_to_2000' => $courtFees->one_defendant_up_to_2000,
                         'two_defendant_up_to_2000' => $courtFees->two_defendant_up_to_2000,
@@ -77,49 +77,52 @@ class FeeDuplicatorController extends Controller
                     ]
                 );
 
-                $doesCivilDetailsExist = CivilUnique::where('court_details_id', $magistrateId)->get();
+                if ($civilFees != '') {
 
-                if ($doesCivilDetailsExist->isEmpty()) {
-                    $civilUnique = new CivilUnique();
-                    $civilUnique->court_details_id = $magistrateId;
-                    $civilUnique->under_500_1_def_mail = $civilFees->under_500_1_def_mail;
-                    $civilUnique->btn_500_2000_1_def_mail = $civilFees->btn_500_2000_1_def_mail;
-                    $civilUnique->btn_2000_4000_1_def_mail = $civilFees->btn_2000_4000_1_def_mail;
-                    $civilUnique->btn_4000_12000_1_def_mail = $civilFees->btn_4000_12000_1_def_mail;
-                    $civilUnique->under_500_2_def_mail = $civilFees->under_500_2_def_mail;
-                    $civilUnique->btn_500_2000_2_def_mail = $civilFees->btn_500_2000_2_def_mail;
-                    $civilUnique->btn_2000_4000_2_def_mail = $civilFees->btn_2000_4000_2_def_mail;
-                    $civilUnique->btn_4000_12000_2_def_mail = $civilFees->btn_4000_12000_2_def_mail;
-                    $civilUnique->under_500_1_def_constable = $civilFees->under_500_1_def_constable;
-                    $civilUnique->btn_500_2000_1_def_constable = $civilFees->btn_500_2000_1_def_constable;
-                    $civilUnique->btn_2000_4000_1_def_constable = $civilFees->btn_2000_4000_1_def_constable;
-                    $civilUnique->btn_4000_12000_1_def_constable = $civilFees->btn_4000_12000_1_def_constable;
-                    $civilUnique->under_500_2_def_constable = $civilFees->under_500_2_def_constable;
-                    $civilUnique->btn_500_2000_2_def_constable = $civilFees->btn_500_2000_2_def_constable;
-                    $civilUnique->btn_2000_4000_2_def_constable = $civilFees->btn_2000_4000_2_def_constable;
-                    $civilUnique->btn_4000_12000_2_def_constable = $civilFees->btn_4000_12000_2_def_constable;
-                    $civilUnique->save();
-                } else {
-                    CivilUnique::where('court_details_id', $magistrateId)->update(
-                        [
-                            'under_500_1_def_mail' => $civilFees->under_500_1_def_mail,
-                            'btn_500_2000_1_def_mail' => $civilFees->btn_500_2000_1_def_mail,
-                            'btn_2000_4000_1_def_mail' => $civilFees->btn_2000_4000_1_def_mail,
-                            'btn_4000_12000_1_def_mail' => $civilFees->btn_4000_12000_1_def_mail,
-                            'under_500_2_def_mail' => $civilFees->under_500_2_def_mail,
-                            'btn_500_2000_2_def_mail' => $civilFees->btn_500_2000_2_def_mail,
-                            'btn_2000_4000_2_def_mail' => $civilFees->btn_2000_4000_2_def_mail,
-                            'btn_4000_12000_2_def_mail' => $civilFees->btn_4000_12000_2_def_mail,
-                            'under_500_1_def_constable' => $civilFees->under_500_1_def_constable,
-                            'btn_500_2000_1_def_constable' => $civilFees->btn_500_2000_1_def_constable,
-                            'btn_2000_4000_1_def_constable' => $civilFees->btn_2000_4000_1_def_constable,
-                            'btn_4000_12000_1_def_constable' => $civilFees->btn_4000_12000_1_def_constable,
-                            'under_500_2_def_constable' => $civilFees->under_500_2_def_constable,
-                            'btn_500_2000_2_def_constable' => $civilFees->btn_500_2000_2_def_constable,
-                            'btn_2000_4000_2_def_constable' => $civilFees->btn_2000_4000_2_def_constable,
-                            'btn_4000_12000_2_def_constable' => $civilFees->btn_4000_12000_2_def_constable
-                        ]
-                    );
+                    $doesCivilDetailsExist = CivilUnique::where('court_details_id', $magistrateId)->get();
+
+                    if ($doesCivilDetailsExist->isEmpty()) {
+                        $civilUnique = new CivilUnique();
+                        $civilUnique->court_details_id = $magistrateId;
+                        $civilUnique->under_500_1_def_mail = $civilFees->under_500_1_def_mail;
+                        $civilUnique->btn_500_2000_1_def_mail = $civilFees->btn_500_2000_1_def_mail;
+                        $civilUnique->btn_2000_4000_1_def_mail = $civilFees->btn_2000_4000_1_def_mail;
+                        $civilUnique->btn_4000_12000_1_def_mail = $civilFees->btn_4000_12000_1_def_mail;
+                        $civilUnique->under_500_2_def_mail = $civilFees->under_500_2_def_mail;
+                        $civilUnique->btn_500_2000_2_def_mail = $civilFees->btn_500_2000_2_def_mail;
+                        $civilUnique->btn_2000_4000_2_def_mail = $civilFees->btn_2000_4000_2_def_mail;
+                        $civilUnique->btn_4000_12000_2_def_mail = $civilFees->btn_4000_12000_2_def_mail;
+                        $civilUnique->under_500_1_def_constable = $civilFees->under_500_1_def_constable;
+                        $civilUnique->btn_500_2000_1_def_constable = $civilFees->btn_500_2000_1_def_constable;
+                        $civilUnique->btn_2000_4000_1_def_constable = $civilFees->btn_2000_4000_1_def_constable;
+                        $civilUnique->btn_4000_12000_1_def_constable = $civilFees->btn_4000_12000_1_def_constable;
+                        $civilUnique->under_500_2_def_constable = $civilFees->under_500_2_def_constable;
+                        $civilUnique->btn_500_2000_2_def_constable = $civilFees->btn_500_2000_2_def_constable;
+                        $civilUnique->btn_2000_4000_2_def_constable = $civilFees->btn_2000_4000_2_def_constable;
+                        $civilUnique->btn_4000_12000_2_def_constable = $civilFees->btn_4000_12000_2_def_constable;
+                        $civilUnique->save();
+                    } else {
+                        CivilUnique::where('court_details_id', $magistrateId)->update(
+                            [
+                                'under_500_1_def_mail' => $civilFees->under_500_1_def_mail,
+                                'btn_500_2000_1_def_mail' => $civilFees->btn_500_2000_1_def_mail,
+                                'btn_2000_4000_1_def_mail' => $civilFees->btn_2000_4000_1_def_mail,
+                                'btn_4000_12000_1_def_mail' => $civilFees->btn_4000_12000_1_def_mail,
+                                'under_500_2_def_mail' => $civilFees->under_500_2_def_mail,
+                                'btn_500_2000_2_def_mail' => $civilFees->btn_500_2000_2_def_mail,
+                                'btn_2000_4000_2_def_mail' => $civilFees->btn_2000_4000_2_def_mail,
+                                'btn_4000_12000_2_def_mail' => $civilFees->btn_4000_12000_2_def_mail,
+                                'under_500_1_def_constable' => $civilFees->under_500_1_def_constable,
+                                'btn_500_2000_1_def_constable' => $civilFees->btn_500_2000_1_def_constable,
+                                'btn_2000_4000_1_def_constable' => $civilFees->btn_2000_4000_1_def_constable,
+                                'btn_4000_12000_1_def_constable' => $civilFees->btn_4000_12000_1_def_constable,
+                                'under_500_2_def_constable' => $civilFees->under_500_2_def_constable,
+                                'btn_500_2000_2_def_constable' => $civilFees->btn_500_2000_2_def_constable,
+                                'btn_2000_4000_2_def_constable' => $civilFees->btn_2000_4000_2_def_constable,
+                                'btn_4000_12000_2_def_constable' => $civilFees->btn_4000_12000_2_def_constable
+                            ]
+                        );
+                    }
                 }
             }
 
