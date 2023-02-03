9ba9e327ef31bea161de20fa108fb5be5491af2f
Andrew Gaidis <agaidis@moneymappress.com>
2023-02-03 14:11:37 -0500

    adjusting text and about us

--- app/Http/Controllers/CountyAdminController.php
+++ app/Http/Controllers/CountyAdminController.php
@@ -23,7 +23,7 @@ class CountyAdminController extends Controller
 
             } catch (Exception $e) {
                 $errorMsg = new ErrorLog();
-                $errorMsg->payload = $e->getMessage() . ' Line #: ' . $e->getLine();
+                $errorMsg->payload = $e->getMessage() . ' jhjhjLine #: ' . $e->getLine();
 
                 $errorMsg->save();
                 return view('countyAdmin');
@@ -33,15 +33,15 @@ class CountyAdminController extends Controller
 
     public function updateInPersonComplaint(Request $request) {
         try {
-//            if ($request->isChecked == true) {
-//                DB::table('county_settings')
-//                    ->where('county', $request->county)
-//                    ->update(['is_allowed_in_person_complaint' => 1]);
-//            } else {
-//                DB::table('county_settings')
-//                    ->where('county', $request->county)
-//                    ->update(['is_allowed_in_person_complaint' => 0]);
-//            }
+            if ($request->isChecked == true) {
+                DB::table('county_settings')
+                    ->where('county', $request->county)
+                    ->update(['is_allowed_in_person_complaint' => 1]);
+            } else {
+                DB::table('county_settings')
+                    ->where('county', $request->county)
+                    ->update(['is_allowed_in_person_complaint' => 0]);
+            }
 
             $request->session()->flash('alert-success',  $request->county . ' County has been updated!');
             return back();
