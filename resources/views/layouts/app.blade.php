95fe090f3dc2a1760f858c6229b1e85d43030624
Andrew Gaidis <agaidis@moneymappress.com>
2023-04-12 00:13:00 -0400

    committing fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
 
     <!-- Scripts -->
     <script src="https://js.stripe.com/v3/"></script>
-{{--    <script src="{{ mix('js/app.js') }}" defer></script>--}}
+    <script src="{{ mix('js/app.js') }}" defer></script>
 
     <script>
         let environmentPath = '{!! env('APP_URL') !!}';
