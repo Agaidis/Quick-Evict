c4364dc2609544cbfcd4037e9d9a102cac5b733a
Andrew Gaidis <agaidis@moneymappress.com>
2023-04-16 15:24:49 -0400

    adding note

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -56,7 +56,7 @@ class DashboardController extends Controller
                 $errorMsg->save();
 
                 $evictions = DB::table('evictions')
-                    ->select('evictions.id', 'users.name AS name', 'user_id', 'property_address', 'status', 'court_details.county', 'file_type', 'is_downloaded', 'owner_name', 'tenant_name', 'court_date', 'total_judgement', 'filing_fee',  'evictions.created_at', 'is_extra_files', 'court_number', 'is_in_person_filing')
+                    ->select('evictions.id', 'users.name AS name', 'user_id', 'property_address', 'status', 'court_details.county', 'file_type', 'is_downloaded', 'owner_name', 'tenant_name', 'court_date', 'total_judgement', 'filing_fee',  'evictions.created_at', 'is_extra_files', 'evictions.court_number', 'is_in_person_filing')
                     ->join('users', 'evictions.user_id', '=', 'users.id')
                     ->join('court_details', 'court_details.magistrate_id', '=', 'evictions.magistrate_id')
                     ->orderBy('evictions.id', 'desc')
