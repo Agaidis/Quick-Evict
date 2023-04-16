887314794f7835433c19cbfeed5402bc96ca5c05
Andrew Gaidis <agaidis@moneymappress.com>
2023-04-16 15:29:19 -0400

    adding note

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -42,23 +42,11 @@ class DashboardController extends Controller
             $courtNumber = Auth::user()->court_id;
             $userRole = Auth::user()->role;
             $counties = CourtDetails::distinct()->orderBy('county')->get(['county']);
-            $notes = '';
 
-      //      $notesArray = array();
             if (Auth::user()->role == 'Administrator') {
-//                $notes = DB::select('select county from county_notes');
-//                foreach ($notes as $note) {
-//                    array_push($notesArray, $note->county);
-//                }
-
-                $errorMsg = new ErrorLog();
-                $errorMsg->payload = serialize($notes);
-                $errorMsg->save();
-
                 $evictions = DB::table('evictions')
-                    ->select('evictions.id', 'users.name AS name', 'user_id', 'property_address', 'status', 'file_type', 'is_downloaded', 'owner_name', 'tenant_name', 'court_date', 'total_judgement', 'filing_fee',  'evictions.created_at', 'is_extra_files', 'evictions.court_number', 'is_in_person_filing')
+                    ->select('evictions.id', 'users.name AS name', 'user_id', 'property_address', 'status', 'file_type', 'is_downloaded', 'owner_name', 'tenant_name', 'court_date', 'total_judgement', 'filing_fee',  'evictions.created_at', 'is_extra_files', 'court_number', 'is_in_person_filing')
                     ->join('users', 'evictions.user_id', '=', 'users.id')
-                 //   ->join('court_details', 'court_details.magistrate_id', '=', 'evictions.magistrate_id')
                     ->orderBy('evictions.id', 'desc')
                     ->take(600)
                     ->get();
@@ -78,7 +66,7 @@ class DashboardController extends Controller
                 }
 
                 $evictions = DB::table('evictions')
-                    ->select('evictions.id', 'users.name AS name', 'user_id', 'property_address', 'status', 'file_type', 'is_downloaded', 'owner_name', 'tenant_name', 'court_date', 'total_judgement', 'filing_fee',  'evictions.created_at', 'is_extra_files', 'evictions.court_number', 'is_in_person_filing')
+                    ->select('evictions.id', 'users.name AS name', 'user_id', 'property_address', 'status', 'file_type', 'is_downloaded', 'owner_name', 'tenant_name', 'court_date', 'total_judgement', 'filing_fee',  'evictions.created_at', 'is_extra_files', 'court_number', 'is_in_person_filing')
                     ->whereIn('user_id', $userIdArr)
                     ->join('users', 'evictions.user_id', '=', 'users.id')
                     ->orderBy('evictions.id', 'desc')
