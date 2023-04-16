83a845773c67d69e7ffa5586d3982f819c26066c
Andrew Gaidis <agaidis@moneymappress.com>
2023-04-16 15:03:08 -0400

    adding note

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -49,8 +49,8 @@ class DashboardController extends Controller
 
                 $errorMsg = new ErrorLog();
                 $errorMsg->payload = serialize($notes);
-
                 $errorMsg->save();
+
                 $evictions = DB::table('evictions')
                     ->select('evictions.id', 'users.name AS name', 'user_id', 'property_address', 'status', 'file_type', 'is_downloaded', 'owner_name', 'tenant_name', 'court_date', 'total_judgement', 'filing_fee',  'evictions.created_at', 'is_extra_files', 'court_number', 'is_in_person_filing')
                     ->join('users', 'evictions.user_id', '=', 'users.id')
