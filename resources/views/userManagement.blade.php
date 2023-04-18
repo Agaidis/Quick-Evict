36f6cb4e16e1d9d1b93468a1d7740b1f165910d5
Andrew Gaidis <agaidis@moneymappress.com>
2023-04-17 22:06:31 -0400

    filing fee

--- resources/views/userManagement.blade.php
+++ resources/views/userManagement.blade.php
@@ -4,7 +4,7 @@
     <meta name="csrf-token" id="token" content="{{ csrf_token() }}">
     <div class="container-fluid">
         <div class="row justify-content-center">
-            <div class="col-md-10">
+            <div class="col-md-12">
                 <div class="card">
                     <div class="card-header"><h2>Manage Users</h2></div>
                     <div class="card-body">
