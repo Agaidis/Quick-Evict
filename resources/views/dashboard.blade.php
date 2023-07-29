3d63a7d8f3a3961cfe7f0cd95886e442b81d67af
Andrew Gaidis <agaidis@moneymappress.com>
2023-07-29 13:35:33 -0400

    adding notes for evictions

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -370,7 +370,7 @@
 
             <!-- Modal -->
             <div class="modal fade" id="seeFilingDetailsModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog modal-xl" role="document">
+                <div class="modal-dialog" style="max-width:850px;" role="document">
                     <div class="modal-content">
                         <div class="modal-header">
                             <h5 class="modal-title" id="exampleModalLabel">Filing Details</h5>
