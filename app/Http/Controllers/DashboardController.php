f5734520ba1d4accfe102fcbfd17fe12c2043089
Andrew Gaidis <agaidis@moneymappress.com>
2023-07-29 14:07:35 -0400

    adding notes for evictions

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -374,7 +374,7 @@ CourtZip Team', '<p>Hello,</p>
 
 
             EvictionNote::where('id', $newEvictionNote->id)
-                ->update(['notes' => '<div class="eviction_note" id="eviction_note_'.$newEvictionNote->id.'"><p style="font-size:14px; margin-bottom:0;"> '.$userName . ' | '. $date . '<span class="fas fa-trash delete_eviction_note" id="delete_eviction_note_'.$newEvictionNote->id.'" style="display:none; cursor:pointer; color:red;"></span></p>' . $request->note .'<hr></div>']);
+                ->update(['note' => '<div class="eviction_note" id="eviction_note_'.$newEvictionNote->id.'"><p style="font-size:14px; margin-bottom:0;"> '.$userName . ' | '. $date . '<span class="fas fa-trash delete_eviction_note" id="delete_eviction_note_'.$newEvictionNote->id.'" style="display:none; cursor:pointer; color:red;"></span></p>' . $request->note .'<hr></div>']);
 
             $currentEvictionNotes = EvictionNote::where('eviction_id', $request->eviction_id)->orderBy('id', 'DESC')->get();
 
