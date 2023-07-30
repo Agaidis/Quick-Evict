95712ef93b0432c11e132f7bdb1e7f234c22deb5
Andrew Gaidis <agaidis@moneymappress.com>
2023-07-30 19:29:56 -0400

    adding notes for evictions

--- app/Http/Controllers/EvictionController.php
+++ app/Http/Controllers/EvictionController.php
@@ -49,8 +49,7 @@ class EvictionController extends Controller
             $evictionData = Evictions::where('id', $request->id)->first();
             $magData = CourtDetails::where('magistrate_id', $evictionData->magistrate_id)->first();
             $mailer = new Mailer();
-            $mailer->sendMail('withdrawal@courtzip.com', 'Please Withdrawal Case (Case #) for user ' . Auth::user()->name,'Email Title: Withdraw 7071-1 (the case #)
-
+            $mailer->sendMail('withdrawal@courtzip.com', 'Please Withdrawal Case ('.$evictionData->id.') for user ' . Auth::user()->name,'
 Filed Date / Time is ' . $evictionData->created_at .',
 Court Date is ' . $evictionData->court_date .',
 Property Address is ' . $evictionData->property_address .',
