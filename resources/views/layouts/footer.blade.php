00035aae79c4061c6819923dad2e4bfd67ee21a5
Andrew Gaidis <agaidis@moneymappress.com>
2023-07-30 19:56:50 -0400

    adding notes for evictions

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -4,9 +4,6 @@
         <div class="row">
             <div class="col-lg-12 h-100 text-center my-auto">
                 <ul class="list-inline mb-2">
-                    <li class="list-inline-item">
-                        <a href="{{ url('about-us') }}">About Us</a>
-                    </li>
                     <li class="list-inline-item">&sdot;</li>
                     <li class="list-inline-item">
                         <a href="{{ url('information') }}">Information</a>
@@ -20,6 +17,7 @@
                         <a href="{{ url('FAQ') }}">FAQ</a>
                     </li>
                 </ul>
+                <p class="text-muted small mb-4 mb-lg-0">&copy; Contact us at info@courtzip.com</p><br>
                 <p class="text-muted small mb-4 mb-lg-0">&copy; CourtZip 2020. All Rights Reserved.</p><br>
                 <p style="text-align: center;" class="text-muted small mb-4 mb-lg-0">DISCLAIMER:  This website is created and maintained by CourtZip, a private entity, and is not owned, operated, or maintained by the Commonwealth of Pennsylvania or the Administrative Office of Pennsylvania Courts. No Legal Advice: Courtzip is not a law firm and Nothing contained in this Website or the Service constitutes legal advice.
                 </p>
