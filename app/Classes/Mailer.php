34b5cd4290ae168cdde000fef4ca0952f1a6a22a
Andrew Gaidis <agaidis@moneymappress.com>
2023-08-01 08:17:27 -0400

    ag_add_wic

--- app/Classes/Mailer.php
+++ app/Classes/Mailer.php
@@ -2,24 +2,31 @@
 
 
 namespace App\Classes;
+use App\ErrorLog;
 use Mailgun\Mailgun;
 
 
 class Mailer
 {
 
-    public function sendMail($mailTo, $subject, $message, $html) {
+    public function sendMail($mailTo, $subject, $message, $html)
+    {
 
+        try {
+            // First, instantiate the SDK with your API credentials
+            $mg = Mailgun::create(env('MAIL_API_KEY')); // For US servers
 
-        // First, instantiate the SDK with your API credentials
-        $mg = Mailgun::create(env('MAIL_API_KEY')); // For US servers
-
-        $mg->messages()->send('mg.courtzip.com', [
-            'from'    => 'Court Zip Administration <service@courtzip.com>',
-            'to'      => $mailTo,
-            'subject' => $subject,
-            'text'    => $message,
-            'html'    => $html
-        ]);
+            $mg->messages()->send('mg.courtzip.com', [
+                'from' => 'Court Zip Administration <service@courtzip.com>',
+                'to' => $mailTo,
+                'subject' => $subject,
+                'text' => $message,
+                'html' => $html
+            ]);
+        } catch (\Exception $e) {
+            $errorMsg = new ErrorLog();
+            $errorMsg->payload = $e->getMessage() . ' ' . $e->getLine();
+            $errorMsg->save();
+        }
     }
 }
