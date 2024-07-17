0c77b183b202ff5c9d1730044390cc66ab026679
Andrew Gaidis <agaidis@moneymappress.com>
2024-07-17 13:45:55 -0400

    init map

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
     <script>
         let environmentPath = '{!! env('APP_URL') !!}';
     </script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAfPLSbGAHZkEd-8DDB0FcGSlhrV9LQMGM&libraries=places&callback=initMap&loading=async" async defer></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAfPLSbGAHZkEd-8DDB0FcGSlhrV9LQMGM&libraries=places&loading=async" async defer></script>
 
     <script type="text/javascript">
        
