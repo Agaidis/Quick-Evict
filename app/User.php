411f77835b9ddff0b62d0faadfec42ad1daed354
Andrew Gaidis <agaidis@moneymappress.com>
2024-08-30 11:09:07 -0400

    adding phon number

--- app/User.php
+++ app/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'role', 'county', 'company', 'court_id', 'pay_type'
+        'name', 'email', 'password', 'role', 'county', 'company', 'court_id', 'pay_type', 'phone_number'
     ];
 
     /**
