72639dd1e29905562eb7b817bddf3012bf6deb60
Andrew Gaidis <agaidis@moneymappress.com>
2024-09-09 08:22:06 -0400

    making it 1000

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -59,7 +59,7 @@ class DashboardController extends Controller
                     ->select('evictions.id', 'users.name AS name', 'user_id', 'property_address', 'status', 'docket_number', 'file_type', 'is_downloaded', 'owner_name', 'tenant_name', 'court_date', 'total_judgement', 'filing_fee',  'evictions.created_at', 'is_extra_files', 'court_number', 'is_in_person_filing', 'is_withdrawn')
                     ->join('users', 'evictions.user_id', '=', 'users.id')
                     ->orderBy('evictions.id', 'desc')
-                    ->take(800)
+                    ->take(1000)
                     ->get();
             } else if (Auth::user()->role == 'PM Company Leader') {
                 $emailAddress = Auth::user()->email;
