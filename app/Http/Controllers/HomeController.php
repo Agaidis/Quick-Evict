51f2499e903084664c58df9ff86105ec52ca8245
Andrew Gaidis <agaidis@moneymappress.com>
2025-09-25 09:12:03 -0400

    remove nate

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,10 +14,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-         if (Auth::user()->email != 'nate@slatehousegroup.com') {
-                Auth::logout();
-                return redirect()->route('login');
-            } 
         $counties = CourtDetails::distinct()->orderBy('county')->get(['county']);
 
         return view('home', compact('counties'));
